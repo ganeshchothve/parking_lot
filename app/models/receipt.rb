5be88bad70a590860b4f0443e4fdc0a1455a364e
Ketan Sabnis <ketan@amuratech.com>
2018-01-26 15:47:33 +0530

    bug fixes

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -1,3 +1,4 @@
+require 'autoinc'
 class Receipt
   include Mongoid::Document
   include Mongoid::Timestamps
@@ -70,10 +71,10 @@ class Receipt
 
   def build_for_hdfc
     payload = ""
-    payload += "merchant_id=#{PAYMENT_PROFILE[:CCAVENUE][:merchantid]}&" 
-    payload += "amount="+self.total_amount.to_s+"&" 
-    payload += "order_id="+self.order_id.to_s+"&" 
-    payload += "currency=INR&" 
+    payload += "merchant_id=#{PAYMENT_PROFILE[:CCAVENUE][:merchantid]}&"
+    payload += "amount="+self.total_amount.to_s+"&"
+    payload += "order_id="+self.order_id.to_s+"&"
+    payload += "currency=INR&"
     payload += "language=EN&"
     if Rails.env.production?
       payload += "redirect_url=http://www.embassyindia.com/payment/hdfc/process_payment/success&"
@@ -89,15 +90,15 @@ class Receipt
 
   def handle_response_for_hdfc(encResponse)
     crypto = Receipt::Crypto.new
-    decResp = crypto.decrypt(encResponse,PAYMENT_PROFILE[:CCAVENUE][:working_key])
-    decResp = decResp.split("&") rescue []
-    decResp.each do |key|
-      if key.from(0).to(key.index("=")-1)=='order_status'
-          self.status = key.from(key.index("=")+1).to(-1).downcase
-          if(self.status == "failure")
-            self.status = "failed"
-          end
-      end
+    decResp = crypto.decrypt(encResponse,PAYMENT_PROFILE[:CCAVENUE][:working_key])
+    decResp = decResp.split("&") rescue []
+    decResp.each do |key|
+      if key.from(0).to(key.index("=")-1)=='order_status'
+        self.status = key.from(key.index("=")+1).to(-1).downcase
+        if(self.status == "failure")
+          self.status = "failed"
+        end
+      end
       if key.from(0).to(key.index("=")-1)=='tracking_id'
         self.tracking_id = key.from(key.index("=")+1).to(-1)
       end
@@ -105,8 +106,8 @@ class Receipt
         self.payment_identifier = key.from(key.index("=")+1).to(-1)
       end
       if key.from(0).to(key.index("=")-1)=='failure_message'
-        self.status_message = key.from(key.index("=")+1).to(-1).downcase 
-      end  
+        self.status_message = key.from(key.index("=")+1).to(-1).downcase
+      end
       if key.from(0).to(key.index("=")-1)=='order_id'
         self.id.to_s == key.from(key.index("=")+1).to(-1)
       end
@@ -115,28 +116,29 @@ class Receipt
   end
 
   class Crypto
-    INIT_VECTOR = (0..15).to_a.pack("C*")    
+    INIT_VECTOR = (0..15).to_a.pack("C*")
     def encrypt(plain_text, key)
-        secret_key =  [Digest::MD5.hexdigest(key)].pack("H*") 
-        cipher = OpenSSL::Cipher::Cipher.new('aes-128-cbc')
-        cipher.encrypt
-        cipher.key = secret_key
-        cipher.iv  = INIT_VECTOR
-        encrypted_text = cipher.update(plain_text) + cipher.final
-        return (encrypted_text.unpack("H*")).first
+      secret_key =  [Digest::MD5.hexdigest(key)].pack("H*")
+      cipher = OpenSSL::Cipher::Cipher.new('aes-128-cbc')
+      cipher.encrypt
+      cipher.key = secret_key
+      cipher.iv  = INIT_VECTOR
+      encrypted_text = cipher.update(plain_text) + cipher.final
+      return (encrypted_text.unpack("H*")).first
     end
+
     def decrypt(cipher_text,key)
-        secret_key =  [Digest::MD5.hexdigest(key)].pack("H*")
-        encrypted_text = [cipher_text].pack("H*")
-        decipher = OpenSSL::Cipher::Cipher.new('aes-128-cbc')
-        decipher.decrypt
-        decipher.key = secret_key
-        decipher.iv  = INIT_VECTOR
-        decrypted_text = (decipher.update(encrypted_text) + decipher.final).gsub(/\0+$/, '')
-        return decrypted_text
+      secret_key =  [Digest::MD5.hexdigest(key)].pack("H*")
+      encrypted_text = [cipher_text].pack("H*")
+      decipher = OpenSSL::Cipher::Cipher.new('aes-128-cbc')
+      decipher.decrypt
+      decipher.key = secret_key
+      decipher.iv  = INIT_VECTOR
+      decrypted_text = (decipher.update(encrypted_text) + decipher.final).gsub(/\0+$/, '')
+      return decrypted_text
     end
   end
-  
+
   private
   def validate_total_amount
     if self.total_amount < ProjectUnit.blocking_amount && self.project_unit_id.blank? && self.new_record?
