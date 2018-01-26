1815a648842c0843a9884aef9e1c7b37b61bf902
Ketan Sabnis <ketan@amuratech.com>
2018-01-26 18:52:50 +0530

    yml path namespaced

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production" || Rails.env == "staging"
-  SIDEKIQ_CONFIG = YAML.load(File.open( "/usr/local/sidekiq.yml" ).read).symbolize_keys
+  SIDEKIQ_CONFIG = YAML.load(File.open( "/usr/local/booking-portal-sidekiq.yml" ).read).symbolize_keys
 end
 Sidekiq.configure_server do |config|
   config.options[:queues] = %w(default mailers) if Rails.env.development? || Rails.env.test?
@@ -9,4 +9,3 @@ end
 Sidekiq.configure_client do |config|
   config.redis = REDIS_CURRENT_CONFIG
 end
-
