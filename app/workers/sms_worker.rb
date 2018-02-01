b04caf898690081632ea7bf0b8ccc95e68b3cd12
Ketan Sabnis <ketan@amuratech.com>
2018-02-01 11:08:10 +0530

    replaced sms worker with pinacle instead of knowlarity

--- app/workers/sms_worker.rb
+++ app/workers/sms_worker.rb
@@ -2,26 +2,31 @@ require 'net/http'
 class SMSWorker
   include Sidekiq::Worker
 
-  def perform(to, content, type="trans")
-    if to.present? && content.present? && Rails.env.production? && SMSWorker.auth_key.present?
-      params = {}
-      headers = {}
-      sms = sms
-      params[:is_file] = 'no'
-      params[:sms_to] = to
-      params[:sms_text] = content
-      headers[:auth_key] = SMSWorker.auth_key
-      params[:sms_from] = SMSWorker.mask
-      params[:sms_type] = (type == 'promotional' ? 'promo' : 'trans')
+  def perform(to, content)
+    params = {}
+    params[:senderid] = SMSWorker.mask
+    params[:dest_mobileno] = to
+    params[:message] = content
+    params[:username] = SMSWorker.username
+    params[:pass] = SMSWorker.password
+    params[:response] = "Y"
 
-      url = "http://etsrds.kapps.in/webapi/amura/api/amura_sms_api.py"
-      params = params.with_indifferent_access
-      headers = headers.with_indifferent_access
-      resp = JSON.parse ::RestClient::Request.execute(method: :post, url: url, payload: params, headers: headers)
+    uri = URI("http://www.smsjust.com/blank/sms/user/urlsms.php")
+    uri.query = URI.encode_www_form(params)
+    response = Net::HTTP.get_response(uri).body
+
+    if response.starts_with?("ES")
+      return {status:"fail", remote_id: response}
+    else
+      return {status:"success", remote_id: response}
     end
   end
 
-  def self.auth_key
+  def self.username
+    ""
+  end
+
+  def self.password
     ""
   end
 
