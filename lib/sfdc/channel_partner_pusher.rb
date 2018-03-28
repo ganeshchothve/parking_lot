e25bc222c135e56a89c37000a36427ebc49fc1d5
pankit <pankit@amuratech.com>
2018-03-28 17:39:14 +0530

    ui fixes and sfdc

---
+++ lib/sfdc/channel_partner_pusher.rb
@@ -0,0 +1,55 @@
+module SFDC
+  class ChannelPartnerPusher < Base
+    def self.execute(channel_partner)
+      if Rails.env.production?
+        channel_partner_id = channel_partner.id
+        begin
+            channel_partner_data = []
+            channel_partner_data << channel_partner_json(channel_partner)
+          if channel_partner_data.any?
+            @channel_partner_pusher = SFDC::Base.new
+            response = @channel_partner_pusher.push("/services/apexrest/Embassy/CPRegistration", channel_partner_data)
+          end
+        rescue Exception => e
+          options = { error_class: e.class, error_message: ('Exception in SFDC ReceiptsCron: ' + e.message), parameters: { client_id: client_id } }
+        end
+      end
+    end
+
+    def self.channel_partner_json(channel_partner)
+        hash = {
+         "cp_id" => channel_partner.id.to_s,
+         "title" => channel_partner.title,
+         "first_name" => channel_partner.first_name, 
+         "last_name" => channel_partner.last_name,
+         "street2" => nil,
+         "street" => channel_partner.street,
+         "street3" => nil,
+         "house_number" => channel_partner.house_number.to_s, 
+         "district" => nil,
+         "city" => channel_partner.city,
+         "postal_code" => channel_partner.postal_code,
+         "country" => channel_partner.country,
+         "region" => nil, #channel_partner.region
+         "telephone" => nil,
+         "mobile_phone" => channel_partner.mobile_phone.to_s,
+         "email" => channel_partner.email, 
+         "company_name" =>  channel_partner.company_name,
+         "pan_no" => channel_partner.pan_no,
+         "gstin_no" => channel_partner.gstin_no, 
+         "rera_id" => channel_partner.rera_id.to_s, 
+         "bank_name" => channel_partner.bank_name,
+         "bank_beneficiary_account_no" => channel_partner.bank_beneficiary_account_no.to_s, 
+         "bank_account_type" => channel_partner.bank_account_type.to_s,
+         "bank_address" =>  channel_partner.bank_address,
+         "bank_city" =>  channel_partner.bank_city,
+         "bank_postal_Code" => channel_partner.bank_postal_Code,
+         "bank_region" => channel_partner.bank_region,
+         "bank_country" => channel_partner.bank_country,
+         "bank_ifsc_code" => channel_partner.bank_ifsc_code.to_s,
+         "bank_phone" => "null"
+      }
+      hash
+    end
+  end
+end
---
+++ lib/sfdc/project_unit_pusher.rb
@@ -0,0 +1,46 @@
+module SFDC
+  class ProjectUnitPusher < Base
+    def self.execute(project_unit)
+      if Rails.env.production?
+        client_id = project_unit.id
+        begin
+          project_unit_data = []
+          project_unit_data << receipt_json(project_unit)
+          if project_unit_data.any?
+           @project_unit_pusher = SFDC::Base.new
+           response = @project_unit_pusher.push("/services/apexrest/Embassy/LeadInfo", project_unit_data)
+          end
+        rescue Exception => e
+          # Rails.logger.info "---------------------------Exception-----------------------------------"
+          options = { error_class: e.class, error_message: ('Exception in SFDC ReceiptsCron: ' + e.message), parameters: { client_id: client_id } }
+          # Rails.logger.info('error', options)
+          # Rails.logger.info("Exception in SFDC::ReceiptsCron >>>> #{e.message} \n #{e.backtrace}", "sfdc_cron.log")
+        end
+      end
+    end
+
+    def self.project_unit_json(project_unit)
+      hash = {
+        "receipt_selldo_id" => receipt.id.to_s,
+        "selldo_lead_id" => receipt.project_unit.user.lead_id,
+        "primary_email" => receipt.user.email,
+        "receipt_date" => sfdc_date_format(receipt.created_at),
+        "payment_amount" => receipt.total_amount,
+        "mode_of_transfer" => receipt.payment_mode,
+        "instrument_no" => receipt.payment_identifier.to_s,
+        "instrument_date" => receipt.issued_date ? sfdc_date_format(Date.parse(receipt.issued_date)) : nil,
+        "bank_name" => receipt.issuing_bank,
+        "branch_name" => receipt.issuing_bank_branch
+
+          "selldo_lead_id": user.lead_id,
+          "booking_stage": "blocked", 
+          "booking_date": "2018-03-26",
+          "birthdate": "1980-10-16",
+          "pan_card_number": "ATZQWE3540P",
+          "unit_sfdc_id": "a0K0l000000jhlk"
+
+      }
+      hash
+    end
+  end
+end
