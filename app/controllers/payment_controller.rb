2bc7fb3411ee08eff6e522d57a2962a39b2535fd
Ketan Sabnis <ketan@amuratech.com>
2018-04-24 18:58:37 +0530

    fixes

--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -6,6 +6,11 @@ class PaymentController < ApplicationController
 
   def process_payment
     @receipt = Receipt.where(receipt_id: params[:receipt_id]).first
-    @receipt.payment_gateway_service.response_handler!(params)
+    if @receipt.payment_gateway_service.present?
+      @receipt.payment_gateway_service.response_handler!(params)
+    else
+      flash[:notice] = 'You are not allowed to access this page'
+      redirect_to after_sign_in_path_for
+    end
   end
 end
