a36127e01c59a16cc61b9353db7bd2a47be48fb1
pbharadiya <parthbharadiya007@gmail.com>
2018-04-25 15:35:19 +0530

    Add payload for non-production servers for SFDC

--- lib/amura_log.rb
+++ lib/amura_log.rb
@@ -1,8 +1,13 @@
 class AmuraLog
-  def self.debug(message=nil, file_name="amura.log")
+  def self.debug(message=nil, file_name="amura.log", options={})
     # To make it more sense, keeping .log in file_name
     file_path = "#{Rails.root}/log/#{file_name}"
     @amura_log ||= Logger.new(file_path)
+    if options[:payload].present?
+      @amura_log.debug("----------------------PAYLOAD-------------------------------") unless message.nil?
+      @amura_log.debug(options[:payload])
+    end
+    @amura_log.debug("------------------------RESPONSE-----------------------------------") unless message.nil?
     @amura_log.debug(message) unless message.nil?
   end
 end
