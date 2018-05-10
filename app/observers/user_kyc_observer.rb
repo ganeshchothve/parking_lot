311f0ea8f84690d93d7a1d0351c516117bf5d151
Ketan Sabnis <ketan@amuratech.com>
2018-05-10 19:24:03 +0530

    bugfix

--- app/observers/user_kyc_observer.rb
+++ app/observers/user_kyc_observer.rb
@@ -1,13 +1,13 @@
 class UserKycObserver < Mongoid::Observer
   def after_create user_kyc
-    mailer = UserKycMailer.send_applicant(user_request.id.to_s)
+    mailer = UserKycMailer.send_applicant(user_kyc.id.to_s)
     if Rails.env.development?
       mailer.deliver
     else
       mailer.deliver_later
     end
 
-    mailer = UserKycMailer.send_user(user_request.id.to_s)
+    mailer = UserKycMailer.send_user(user_kyc.id.to_s)
     if Rails.env.development?
       mailer.deliver
     else
