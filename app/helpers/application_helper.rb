55f5258904bc573df0b571d06f63929bc4386a50
Ketan Sabnis <ketan@amuratech.com>
2018-05-18 09:10:12 +0530

    fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,10 +13,8 @@ module ApplicationHelper
       number = string[0].gsub(/(\d+)(\d{3})$/){ p = $2;"#{$1.reverse.gsub(/(\d{2})/,'\1,').reverse},#{p}"}
       number = number.gsub(/^,/, '')
       number = number + '.' + string[1] if string[1].to_f > 0
-      # number = (number.to_i).round(2)
-      
     end
-    "₹#{number}"
+    "&#8377;#{number}".html_safe
   end
 
   def float_to_int (x)
@@ -28,7 +26,7 @@ module ApplicationHelper
   end
 
   def calculate_percent(amount, percent)
-    amount = amount * percent/100 
+    amount = amount * percent/100
     amount.round
   end
 end
