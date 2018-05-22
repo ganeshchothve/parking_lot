9a878065063f04f903c7f70f1b808b38773bc030
Ketan Sabnis <ketan@amuratech.com>
2018-05-22 17:20:35 +0530

    fix

--- config/initializers/razorpay.rb
+++ config/initializers/razorpay.rb
@@ -1,2 +1,6 @@
 require "razorpay"
-Razorpay.setup('rzp_test_NTQGRS3ia0hiWY', 'pzM04pY4CJFkHbM3iWKBjDhN')
+if Rails.env.production?
+  Razorpay.setup(ENV_CONFIG['razorpay']['key'], ENV_CONFIG['razorpay']['secret'])
+else
+  Razorpay.setup('rzp_test_bMV0Fp1FW4KQwy', 'obAjJRSWKB4k5dxAazcQeivw')
+end
