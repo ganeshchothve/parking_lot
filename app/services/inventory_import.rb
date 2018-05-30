b73678a49f1f3b7568bf83414cc8accf96d7a700
Ketan Sabnis <ketan@amuratech.com>
2018-05-30 10:14:00 +0530

    import to update inventory written

--- app/services/inventory_import.rb
+++ app/services/inventory_import.rb
@@ -1,4 +1,80 @@
 module InventoryImport
+  def self.update(filepath)
+    count = 0
+    CSV.foreach(filepath) do |row|
+      unless count == 0
+        sfdc_id = row[0].strip
+        project_name = row[1].strip
+        project_tower_name = row[2].strip
+        unit_name = row[3].strip
+        unit_number = row[4].strip
+        floor = row[5].strip
+        carpet = row[6].strip.to_f.round(2)
+        saleable = row[7].strip.to_f.round(2)
+        base_rate = row[8].strip
+        category = row[9].strip
+        unit_configuration_name = row[10].strip
+        record_type = row[11].strip
+
+        infrastructure_charges = row[12].strip
+        power_supply = row[13].strip
+        clubhouse_amenities_price = row[14].strip
+        corpus_fund_charges = row[15].strip
+        premium_location_charges = row[16].strip
+        floor_rise = row[17].strip
+        unit_sap_id = row[18]
+        status = row[19].strip
+        maintenance_deposit = row[20].strip
+        bedrooms = row[21].strip
+        bathrooms = row[22].strip
+        agreement_price = row[23].strip
+        land_rate = row[24].strip
+        unit_facing_direction = row[25].strip
+        usable = row[26].strip
+        uds = row[26].strip
+
+        client_id = ENV_CONFIG['selldo']['client_id'] || "531de108a7a03997c3000002"
+
+        project_unit = ProjectUnit.in(status: ["available", "employee", "management"]).where(sfdc_id: sfdc_id)
+        if project_unit.present?
+          project_unit.sfdc_id = sfdc_id
+          project_unit.name = "#{unit_name} | #{unit_configuration_name}"
+          if status == "Available"
+            project_unit.status = "available"
+            project_unit.available_for = "user"
+          elsif status == "Management Blocking"
+            project_unit.status = "management"
+            project_unit.available_for = "management"
+          elsif status == "Employee Blocking"
+            project_unit.status = "employee"
+            project_unit.available_for = "employee"
+          else
+            project_unit.status = "error"
+            project_unit.available_for = "user"
+          end
+          project_unit.base_rate = base_rate.to_f
+          project_unit.client_id = client_id
+          project_unit.data_attributes = [{"n"=>"unit_configuration_id", "v"=>"#{unit_configuration.id}"}, {"n"=>"project_name", "v"=>"#{project_name}"}, {"n"=>"project_tower_name", "v"=>"#{project_tower_name}"}, {"n"=>"unit_configuration_name", "v"=>"#{unit_configuration_name}"}, {"n"=>"floor", "v"=> floor}, {"n"=>"resale", "v"=>false}, {"n"=>"bedrooms", "v"=>bedrooms.to_f}, {"n"=>"bathrooms", "v"=>bathrooms.to_f}, {"n"=>"category", "v"=>category}, {"n"=>"facing", "v"=>unit_facing_direction}, {"n"=>"type", "v"=>"apartment"}, {"n"=>"saleable", "v"=>saleable.to_f}, {"n"=>"carpet", "v"=>carpet.to_f}, {"n"=>"usable", "v"=>usable.to_f}, {"n"=>"uds", "v"=>uds.to_f}, {"n"=>"city", "v"=>"Banglore"}, {"n"=>"state", "v"=>"Karnataka"}, {"n"=>"country", "v"=>"India"}, {"n"=>"amenities", "v"=>{}}, {"n"=>"project_status", "v"=>nil}]
+          project_unit.selldo_id = unit_sap_id # TODO
+          project_unit.agreement_price = agreement_price.to_f
+          project_unit.clubhouse_amenities_price = clubhouse_amenities_price
+          project_unit.premium_location_charges = premium_location_charges.to_f
+          project_unit.floor_rise = floor_rise.to_f
+          project_unit.land_rate = land_rate.to_f
+
+          if project_unit.save
+            puts "Saved #{project_unit.name}"
+          else
+            puts "Error in saving #{project_unit.name} : #{project_unit.errors.full_messages}"
+          end
+        else
+          puts "Not updating project unit: #{unit_name} - #{sfdc_id}"
+        end
+      end
+      count += 1
+    end
+  end
+
   def self.perform(filepath)
     count = 0
     CSV.foreach(filepath) do |row|
