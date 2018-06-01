eaf8c87ed95c00d5880c2534d2c8b60d3822447e
Ketan Sabnis <ketan@amuratech.com>
2018-06-01 13:36:41 +0530

    fixes

--- app/mailers/user_reminder_mailer.rb
+++ app/mailers/user_reminder_mailer.rb
@@ -3,9 +3,9 @@ class UserReminderMailer < ApplicationMailer
   def daily_reminder_for_booking_payment project_unit_id
     @project_unit = ProjectUnit.find(project_unit_id)
     @user = @project_unit.user
-    # TODO: handle specific case if its already 6 days - mention we will release the unit tomorrow
     @cp = @user.channel_partner
     cc = @cp.present? ? [@cp.email] : []
+    @days = (@project_unit.auto_release_on - Date.today).to_i
     mail(to: @user.email, cc: cc, subject: subject)
   end
 end
--- app/services/project_unit_reminders_and_auto_release.rb
+++ app/services/project_unit_reminders_and_auto_release.rb
@@ -2,17 +2,22 @@ module ProjectUnitRemindersAndAutoRelease
   class Job
     def self.daily_reminder_for_booking_payment
       ProjectUnit.in(status: ["blocked", 'booked_tentative']).where(auto_release_on: {"$gte" => Date.today}).each do |project_unit|
-        # mailer = UserReminderMailer.daily_reminder_for_booking_payment(project_unit.id.to_s)
-        # if Rails.env.development?
-        #   mailer.deliver
-        # else
-        #   mailer.deliver_later
-        # end
+        days = (project_unit.auto_release_on - Date.today).to_i
+        if [9,7,5,3,2,1].include?(days)
+          mailer = UserReminderMailer.daily_reminder_for_booking_payment(project_unit.id.to_s)
+          if Rails.env.development?
+            mailer.deliver
+          else
+            mailer.deliver_later
+          end
+        end
         message = project_unit.promote_future_payment_message
-        if Rails.env.development?
-          SMSWorker.new.perform(project_unit.user.phone.to_s, message)
-        else
-          SMSWorker.perform_async(project_unit.user.phone.to_s, message)
+        if message.present?
+          if Rails.env.development?
+            SMSWorker.new.perform(project_unit.user.phone.to_s, message)
+          else
+            SMSWorker.perform_async(project_unit.user.phone.to_s, message)
+          end
         end
       end
     end
