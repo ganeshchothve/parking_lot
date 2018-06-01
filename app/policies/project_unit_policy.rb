84c79d15d6b9c7bd435bb43c6b8f46cab47614e2
Ketan Sabnis <ketan@amuratech.com>
2018-06-01 11:38:47 +0530

    fix

--- app/policies/project_unit_policy.rb
+++ app/policies/project_unit_policy.rb
@@ -4,7 +4,7 @@ class ProjectUnitPolicy < ApplicationPolicy
   end
 
   def edit?
-    (record.auto_release_on.present? || ["available", "not_available", "employee", "management"].include?(record.status)) && ['crm', 'admin'].include?(user.role)
+    ((['blocked', 'booked_tentative', 'booked_confirmed', 'error'].include?(record) && record.auto_release_on.present?) || ["available", "not_available", "employee", "management"].include?(record.status)) && ['crm', 'admin'].include?(user.role)
   end
 
   def eoi?
