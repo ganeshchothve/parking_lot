c742852bb7604b52eff2b606e6e4b6c1c59021c3
pankit <pankit@amuratech.com>
2018-06-08 16:14:57 +0530

    fixes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -21,7 +21,7 @@ class HomeController < ApplicationController
           else
             @user = User.or([{email: params['email']}, {phone: params['phone']}, {lead_id: params['lead_id']}]).first #TODO: check if you want to find uniquess on lead id also
             if @user.present?
-              message = 'A user with these details has already registered'
+              flash[:notice]  = 'A user with these details has already registered'
               if (!@user.confirmed? || (@user.channel_partner_id.blank? && @user.booking_details.blank?)) && @user.role?('user')
                 if current_user.present? && current_user.role?('channel_partner')
                   @user.set(referenced_channel_partner_ids: [current_user.id], channel_partner_id: current_user.id)
