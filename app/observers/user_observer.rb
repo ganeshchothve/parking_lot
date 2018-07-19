359cb59555e7d4151f4cfe1d3f17461dc3977cfe
vrushaliw <vrushali@amuratech.com>
2018-07-19 15:35:18 +0530

    Commenting code for sending channel partner an email that his/her lead is activated and associated with other channel partner as per request

--- app/observers/user_observer.rb
+++ app/observers/user_observer.rb
@@ -19,15 +19,15 @@ class UserObserver < Mongoid::Observer
       user.referenced_channel_partner_ids << user.channel_partner_id
     end
     if user.confirmed_at_changed?
-      channel_partner_ids = user.referenced_channel_partner_ids - [user.channel_partner_id]
-      channel_partner_ids.each do |channel_partner_id|
-        mailer = ChannelPartnerMailer.send_user_activated_with_other(channel_partner_id, user.id)
-        if Rails.env.development?
-          mailer.deliver
-        else
-          mailer.deliver_later
-        end
-      end
+      # channel_partner_ids = user.referenced_channel_partner_ids - [user.channel_partner_id]
+      # channel_partner_ids.each do |channel_partner_id|
+      #   mailer = ChannelPartnerMailer.send_user_activated_with_other(channel_partner_id, user.id)
+      #   if Rails.env.development?
+      #     mailer.deliver
+      #   else
+      #     mailer.deliver_later
+      #   end
+      # end
       user.referenced_channel_partner_ids = [user.channel_partner_id]
     end
     unless user.authentication_token?
