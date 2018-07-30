bcf9d8def2c1653ea580bd8c556fee29fc26bb12
Ketan Sabnis <ketan@amuratech.com>
2018-07-30 12:18:29 +0530

    fixes

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,16 +1,13 @@
 CarrierWave.configure do |config|
-  if Rails.env.production? || Rails.env.staging?
-    config.storage = Fog
-    config.fog_directory  = ENV_CONFIG[:asset_sync]['FOG_DIRECTORY']
-    config.fog_public     = false
-    config.fog_attributes = {'Cache-Control' => 'max-age=315576000'}
-
-    config.fog_credentials = {
-      provider:                'AWS',
-      aws_access_key_id:       ENV_CONFIG[:asset_sync]['AWS_SECRET_ACCESS_KEY'],
-      aws_secret_access_key:   ENV_CONFIG[:asset_sync]['AWS_ACCESS_KEY_ID'],
-      aws_signature_version:   2,
-      region:                  ENV_CONFIG[:asset_sync]['FOG_REGION']
-    }
-  end
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+    provider:              'AWS',
+    aws_access_key_id:     ENV_CONFIG[:asset_sync]['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV_CONFIG[:asset_sync]['AWS_SECRET_ACCESS_KEY'],
+    use_iam_profile:       true,
+    region:                ENV_CONFIG[:asset_sync]['FOG_REGION']
+  }
+  config.fog_directory  = ENV_CONFIG[:asset_sync]['FOG_DIRECTORY']
+  config.fog_public     = true
+  config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
 end
