d5d2e18f153621294e9925cb862f00630e8c98f8
Ketan Sabnis <ketan@amuratech.com>
2018-07-30 17:03:55 +0530

    fixes

--- app/observers/channel_partner_observer.rb
+++ app/observers/channel_partner_observer.rb
@@ -1,4 +1,5 @@
 class ChannelPartnerObserver < Mongoid::Observer
+  include ApplicationHelper
   def after_create channel_partner
     ApplicationLog.log("channel_partner_registered", {id: channel_partner.id}, RequestStore.store[:logging])
     ChannelPartnerMailer.send_create(channel_partner.id)
@@ -7,7 +8,7 @@ class ChannelPartnerObserver < Mongoid::Observer
   def before_save channel_partner
     # register user and set the user's id on the channel partner
     if channel_partner.status_changed? && channel_partner.status == 'active'
-      user = User.create!(first_name: channel_partner.first_name, last_name: channel_partner.last_name, email: channel_partner.email, phone: channel_partner.phone, rera_id: channel_partner.rera_id, role: 'channel_partner')
+      user = User.create!(first_name: channel_partner.first_name, last_name: channel_partner.last_name, email: channel_partner.email, phone: channel_partner.phone, rera_id: channel_partner.rera_id, role: 'channel_partner', booking_portal_client_id: current_client.id)
       # RegistrationMailer.welcome(user, generated_password).deliver #TODO: enable this. We might not need this if we are to use OTP based login
 
       ApplicationLog.log("channel_partner_approved", {id: channel_partner.id}, RequestStore.store[:logging])
