5f5ab8841dd8e4b1a9cbfbbc226965fba3fcbf9a
Ketan Sabnis <ketan@amuratech.com>
2018-07-30 13:11:06 +0530

    fixes

--- app/models/client.rb
+++ app/models/client.rb
@@ -8,7 +8,7 @@ class Client
   field :selldo_api_key, type: String
   field :selldo_default_srd, type: String
   field :cp_srd, type: String
-  field :sfdc_enabled?, type: Boolean
+  field :sfdc_enabled, type: Boolean
   field :helpdesk_number, type: String
   field :helpdesk_email, type: String
   field :notification_email, type: String
@@ -21,15 +21,10 @@ class Client
   field :support_email, type: String
   field :channel_partner_support_number, type: String
   field :channel_partner_support_email, type: String
-  field :payment_gateway_credentials, type: Hash, default: {} # merchantid, accesscode, working_key
   field :erp, type: Hash, default: {} # name, access_token, instance_url, client_id, username, password, client_secret
   field :cancellation_amount, type: Float
   field :area_unit, type: String, default: "sqft"
   field :preferred_login, type: String, default: "phone"
-  field :pusher_api_key, type: String
-  field :pusher_api_secret, type: String
-  field :pusher_api_app_id, type: String
-  field :pusher_api_cluster, type: String
   field :mixpanel_token, type: String
   field :sms_provider_username, type: String
   field :sms_provider_password, type: String
@@ -43,7 +38,7 @@ class Client
   has_many :projects
   has_one :address, as: :addressable
 
-  validate :name, :selldo_client_id, :selldo_form_id, :helpdesk_email, :helpdesk_number, :notification_email, :email_domains, :booking_portal_domains, :registration_name, :website_link, :support_email, :support_number, :preferred_login
+  validate :name, :selldo_client_id, :selldo_form_id, :helpdesk_email, :helpdesk_number, :notification_email, :email_domains, :booking_portal_domains, :registration_name, :website_link, :support_email, :support_number
 
   def self.available_preferred_logins
     [
@@ -51,8 +46,35 @@ class Client
       {id: 'email', text: 'Email Based'}
     ]
   end
-  validates :preferred_login, inclusion: {in: Client.available_preferred_logins }, allow_blank: true
+  validates :preferred_login, inclusion: {in: Proc.new{ ProjectUnit.available_preferred_logins.collect{|x| x[:id]} } }
 end
 
-
-# Client.create(name: "Amanora", notification_email: "no-reply@amanora.com", email_domains: ["amanora.com"], booking_portal_domains: ["booking.amanora.com"])
+=begin
+c = Client.new
+c.name = "Amura"
+c.selldo_client_id = "531de108a7a03997c3000002"
+c.selldo_form_id = "5abba073923d4a567f880952"
+c.selldo_api_key = "bcdd92826cf283603527bd6d832d16c4"
+c.selldo_default_srd = "5a72c7a67c0dac7e854aca9e"
+c.cp_srd = "5a72c7a67c0dac7e854aca9e"
+c.helpdesk_number = "9922410908"
+c.helpdesk_email = "supriya@amuratech.com"
+c.notification_email = "supriya@amuratech.com"
+c.email_domains = ["amuratech.com"]
+c.booking_portal_domains = ["bookingportal.withamura.com"]
+c.registration_name = "Amura Marketing Technologies Pvt. Ltd."
+c.website_link = "www.amuratech.com"
+c.cp_disclaimer = "CP Disclaimer"
+c.support_number = "9922410908"
+c.support_email = "supriya@amuratech.com"
+c.channel_partner_support_number = "9922410908"
+c.channel_partner_support_email = "supriya@amuratech.com"
+c.cancellation_amount = 5000
+c.area_unit = "psqft."
+c.preferred_login = "phone"
+c.sms_provider_username = "amuramarketing"
+c.sms_provider_password = "aJ_Z-1j4"
+c.enable_actual_inventory = false
+c.enable_channel_partners = false
+c.remote_logo_url = "https://image4.owler.com/logo/amura_owler_20160227_194208_large.png"
+=end
