0dcf0674c36c56ee6afde184dfb96a0dda9f4dcd
Ketan Sabnis <ketan@amuratech.com>
2018-07-31 22:36:41 +0530

    fixes

--- app/models/bank_detail.rb
+++ app/models/bank_detail.rb
@@ -7,6 +7,7 @@ class BankDetail
   field :branch, type: String
   field :account_type, type: String
   field :ifsc_code, type: String
+  field :account_holder_name, type: String
   field :account_number, type: String
   field :loan_required, type: Boolean, default: false
 
