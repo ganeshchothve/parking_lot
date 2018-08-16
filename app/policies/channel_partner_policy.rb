1aa1b9c5ebe77677f53ec5a6ba9404f3ea62ed39
Ketan Sabnis <ketan@amuratech.com>
2018-08-16 14:48:56 +0530

    fixes

--- app/policies/channel_partner_policy.rb
+++ app/policies/channel_partner_policy.rb
@@ -1,10 +1,10 @@
 class ChannelPartnerPolicy < ApplicationPolicy
   def index?
-    current_client.enable_channel_partners? && ['superadmin', 'admin', 'cp_admin'].include?(user.role)
+    current_client.enable_channel_partners? && ['superadmin', 'admin', 'cp_admin', 'cp'].include?(user.role)
   end
 
   def export?
-    current_client.enable_channel_partners? && ['superadmin', 'admin', 'cp_admin'].include?(user.role)
+    current_client.enable_channel_partners? && ['superadmin', 'admin', 'cp_admin', 'cp'].include?(user.role)
   end
 
   def new?
@@ -12,7 +12,7 @@ class ChannelPartnerPolicy < ApplicationPolicy
   end
 
   def edit?
-    current_client.enable_channel_partners? && ['superadmin', 'admin', 'cp_admin'].include?(user.role)
+    current_client.enable_channel_partners? && ['superadmin', 'admin', 'cp_admin', 'cp'].include?(user.role)
   end
 
   def create?
@@ -20,12 +20,13 @@ class ChannelPartnerPolicy < ApplicationPolicy
   end
 
   def update?
-    current_client.enable_channel_partners? && ['superadmin', 'admin', 'cp_admin'].include?(user.role)
+    current_client.enable_channel_partners? && ['superadmin', 'admin', 'cp_admin', 'cp'].include?(user.role)
   end
 
   def permitted_attributes params={}
     attributes = [:name, :email, :phone, :rera_id, :title, :first_name, :region, :last_name, :street, :house_number, :city, :postal_code, :country, :mobile_phone, :email, :company_name, :pan_number, :gstin_number, :aadhaar, :rera_id, :manager_id, bank_detail_attributes: BankDetailPolicy.new(user, BankDetail.new).permitted_attributes, address_attributes: AddressPolicy.new(user, Address.new).permitted_attributes]
     attributes += [:status] if user.present? && ['superadmin', 'admin', 'cp_admin'].include?(user.role) && record.status != "active"
+    attributes += [:status] if user.present? && ['cp'].include?(user.role) && record.status != "active" && record.manager_id == user.id
     attributes
   end
 end
