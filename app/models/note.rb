36cecef146f04a971fa5e226797d0c2847e7a833
Ketan Sabnis <ketan@amuratech.com>
2018-08-18 13:01:04 +0530

    fixes

--- app/models/note.rb
+++ app/models/note.rb
@@ -6,6 +6,9 @@ class Note
 
   belongs_to :notable, polymorphic: true
   has_many :assets, as: :assetable
+  belongs_to :creator, class_name: 'User'
 
+  default_scope -> {desc(:created_at)}
+  
   validates :note, presence: true
 end
