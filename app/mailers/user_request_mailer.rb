8ae626ede1944ead325759c022cdba4e4a1a0575
Ketan Sabnis <ketan@amuratech.com>
2018-08-20 17:50:50 +0530

    mailer updates

--- app/mailers/user_request_mailer.rb
+++ app/mailers/user_request_mailer.rb
@@ -5,7 +5,7 @@ class UserRequestMailer < ApplicationMailer
     @project_unit = @user_request.project_unit
     @cp = @user.manager
     cc = @cp.present? ? [@cp.email] : []
-    mail(to: @user.email, cc: cc, subject: "Cancellation Requested for Unit: #{@project_unit.name}")
+    make_bootstrap_mail(to: @user.email, cc: cc, subject: "Cancellation Requested for Unit: #{@project_unit.name}")
   end
 
   def send_resolved user_request_id
@@ -14,7 +14,7 @@ class UserRequestMailer < ApplicationMailer
     @project_unit = @user_request.project_unit
     @cp = @user.manager
     cc = @cp.present? ? [@cp.email] : []
-    mail(to: @user.email, cc: cc, subject: "Cancellation Request for Unit: #{@project_unit.name} Resolved")
+    make_bootstrap_mail(to: @user.email, cc: cc, subject: "Cancellation Request for Unit: #{@project_unit.name} Resolved")
   end
 
   def send_swapped user_request_id
@@ -24,6 +24,6 @@ class UserRequestMailer < ApplicationMailer
     @alternate_project_unit = @user_request.alternate_project_unit
     @cp = @user.manager
     cc = @cp.present? ? [@cp.email] : []
-    mail(to: @user.email, cc: cc, subject: "Swap request resolved for Unit: #{@project_unit.name} with new unit #{@alternate_project_unit.name}")
+    make_bootstrap_mail(to: @user.email, cc: cc, subject: "Swap request resolved for Unit: #{@project_unit.name} with new unit #{@alternate_project_unit.name}")
   end
 end
