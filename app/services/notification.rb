eab8d6947f20ab2116b18f4dcdd0c9b9d1d20bdc
Vrushali Waykole <vrushaliwaykole@Vrushalis-MacBook-Pro.local>
2018-08-24 15:38:55 +0530

    Receipt notification fixes

---
+++ app/services/notification.rb
@@ -0,0 +1,33 @@
+module Notification
+  module Sms
+    def self.execute params={}
+      params = params.with_indifferent_access
+
+      keys = ["booking_portal_client_id", "template_name", "recipient_id", "triggered_by_id", "triggered_by_type"].select do |key|
+        params[key].blank?
+      end
+
+      fail "#{keys} required to send notification" if keys.present?
+
+      template_name = params.delete "template_name"
+
+      ::Sms.create!(params.merge(sms_template_id: Template::SmsTemplate.where(booking_portal_client_id: params[:booking_portal_client_id]).find_by(name: template_name).id))
+    end
+  end
+
+  module Email
+    def self.execute params={}
+      params = params.with_indifferent_access
+
+      keys = ["booking_portal_client_id", "template_name", "recipient_ids", "triggered_by_id", "triggered_by_type"].select do |key|
+        params[key].blank?
+      end
+
+      fail "#{keys} required to send notification" if keys.present?
+
+      template_name = params.delete "template_name"
+
+      ::Email.create!(params.merge(email_template_id: Template::EmailTemplate.where(booking_portal_client_id: params[:booking_portal_client_id]).find_by(name: template_name).id))
+    end
+  end
+end
