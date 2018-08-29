971b4afa774691f1ab8a81bdda2ab9c5a54aea45
Ketan Sabnis <ketan@amuratech.com>
2018-08-29 16:07:48 +0530

    fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -345,7 +345,7 @@ class User
   end
 
   def unused_user_kyc_ids project_unit_id
-    if self.booking_portal_client.allow_multiple_bookings_per_user_kyc
+    if self.booking_portal_client.allow_multiple_bookings_per_user_kyc?
       user_kyc_ids = self.user_kycs.collect{|x| x.id}
     else
       user_kyc_ids = self.user_kycs.collect{|x| x.id}
