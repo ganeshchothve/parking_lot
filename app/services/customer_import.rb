1b060e96f94f51b2dafd6ae5df3d01b759c745ff
Ketan Sabnis <ketan@amuratech.com>
2018-08-31 20:12:18 +0530

    fixes

--- app/services/customer_import.rb
+++ app/services/customer_import.rb
@@ -3,13 +3,13 @@ module CustomerImport
     count = 0
     booking_portal_client = Client.first
     CSV.foreach(filepath) do |row|
-      unless count == 0
+      if count != 0 && row[0].present?
         lead_id = row[0].strip
         first_name = row[1].present? ? row[1].strip : "Customer"
         last_name = row[2].present? ? row[2].strip : "."
-        email = row[3].present? ? row[3].strip : ""
-        phone = row[4].present? ? "+91#{row[4].strip}" : ""
-        manager_email = row[4].present? ? row[4].strip : ""
+        email = row[4].present? ? row[4].strip : ""
+        phone = row[3].present? ? "+91#{row[3].strip}" : ""
+        manager_email = row[5].present? ? row[5].strip : ""
         manager = User.nin(role: User.buyer_roles(booking_portal_client)).where(email: manager_email).first
         query = []
         query << {email: email} if email.present?
@@ -17,11 +17,11 @@ module CustomerImport
         query << {lead_id: lead_id} if lead_id.present?
         customer = User.where(role: "customer").or(query).first
         if customer.present?
-          # puts customer.id
+          puts "Found #{customer.id}"
           # customer.update({first_name: first_name, last_name: last_name, phone: phone, email: email, manager_id: manager})
         else
           user = User.new({lead_id: lead_id, first_name: first_name, last_name: last_name, phone: phone, email: email, booking_portal_client: booking_portal_client})
-          user.manager = manager if manager.present?
+          user.manager_id = manager.id if manager.present?
           unless user.valid?
             puts "#{user.errors.full_messages} #{user.lead_id} #{user.name} #{user.email} #{user.phone}"
           end
