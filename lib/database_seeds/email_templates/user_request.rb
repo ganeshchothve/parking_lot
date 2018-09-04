3c575d5c8785b454fd29bef95e5fad20fb9abe48
Ketan Sabnis <ketan@amuratech.com>
2018-09-04 15:35:57 +0530

    fixes

--- lib/database_seeds/email_templates/user_request.rb
+++ lib/database_seeds/email_templates/user_request.rb
@@ -2,7 +2,7 @@ module DatabaseSeeds
   module EmailTemplates
     module UserRequest
       def self.seed client_id
-        Template::EmailTemplate.create!(booking_portal_client_id: client_id, subject_class: "UserRequest", name: "cancellation_request_created", subject: "Cancellation Requested for Unit: <%= self.project_unit.name %>", content: '<div class="card w-100">
+        Template::EmailTemplate.create!(booking_portal_client_id: client_id, subject_class: "UserRequest::Cancellation", name: "cancellation_request_created", subject: "Cancellation Requested for Unit: <%= self.project_unit.name %>", content: '<div class="card w-100">
           <div class="card-body">
             <p>Dear <%= self.user.name %>,</p>
             <p>
@@ -11,7 +11,7 @@ module DatabaseSeeds
           </div>
         </div>') if ::Template::EmailTemplate.where(name: "cancellation_request_created").blank?
 
-        Template::EmailTemplate.create!(booking_portal_client_id: client_id, subject_class: "UserRequest", name: "cancellation_request_resolved", subject: "Cancellation Request for Unit: <%= self.project_unit.name %> Resolved", content: '<div class="card w-100">
+        Template::EmailTemplate.create!(booking_portal_client_id: client_id, subject_class: "UserRequest::Cancellation", name: "cancellation_request_resolved", subject: "Cancellation Request for Unit: <%= self.project_unit.name %> Resolved", content: '<div class="card w-100">
           <div class="card-body">
             <p>Dear <%= self.user.name %>,</p>
             <p>
@@ -22,9 +22,9 @@ module DatabaseSeeds
           </div>
         </div>') if ::Template::EmailTemplate.where(name: "cancellation_request_resolved").blank?
 
-        Template::EmailTemplate.create!(booking_portal_client_id: client_id, subject_class: "UserRequest", name: "swap_request_created", subject: "swap Requested for Unit: <%= self.project_unit.name %>", content: '<%= self.project_unit.name %>') if ::Template::EmailTemplate.where(name: "swap_request_created").blank?
+        Template::EmailTemplate.create!(booking_portal_client_id: client_id, subject_class: "UserRequest::Swap", name: "swap_request_created", subject: "swap Requested for Unit: <%= self.project_unit.name %>", content: '<%= self.project_unit.name %>') if ::Template::EmailTemplate.where(name: "swap_request_created").blank?
 
-        Template::EmailTemplate.create!(booking_portal_client_id: client_id, subject_class: "UserRequest", name: "swap_request_resolved", subject: "swap Request for Unit: <%= self.project_unit.name %> Resolved", content: '<%= self.project_unit.name %>') if ::Template::EmailTemplate.where(name: "swap_request_resolved").blank?
+        Template::EmailTemplate.create!(booking_portal_client_id: client_id, subject_class: "UserRequest::Swap", name: "swap_request_resolved", subject: "swap Request for Unit: <%= self.project_unit.name %> Resolved", content: '<%= self.project_unit.name %>') if ::Template::EmailTemplate.where(name: "swap_request_resolved").blank?
       end
     end
   end
