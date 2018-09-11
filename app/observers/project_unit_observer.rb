55e72f800d9d9e08e36dffaa5365d06962a669da
Ketan Sabnis <ketan@amuratech.com>
2018-09-11 23:53:04 +0530

    fixes

--- app/observers/project_unit_observer.rb
+++ app/observers/project_unit_observer.rb
@@ -1,8 +1,10 @@
 class ProjectUnitObserver < Mongoid::Observer
   def before_validation project_unit
-    project_unit.agreement_price = project_unit.calculate_agreement_price if project_unit.agreement_price.blank? || project_unit.agreement_price.zero?
-    project_unit.all_inclusive_price = project_unit.calculate_all_inclusive_price if project_unit.all_inclusive_price.blank? || project_unit.all_inclusive_price.zero?
-    project_unit.booking_price = project_unit.agreement_price * project_unit.booking_price_percent_of_agreement_price if project_unit.booking_price.blank? || project_unit.booking_price.zero?
+    project_unit.agreement_price = project_unit.calculate_agreement_price.round
+    project_unit.all_inclusive_price = project_unit.calculate_all_inclusive_price.round
+    if project_unit.agreement_price_changed?
+      project_unit.booking_price = (project_unit.agreement_price * project_unit.booking_price_percent_of_agreement_price).round
+    end
 
     if project_unit.pending_balance.present? && project_unit.pending_balance <= 0
       project_unit.status = "booked_confirmed"
