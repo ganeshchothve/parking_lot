b34b0aa0ab088bff7c37badab1f1a51e26c242e3
Vrushali Waykole <vrushali@amuratech.com>
2018-10-10 15:30:41 +0530

    Inventory view

--- app/policies/custom_policy.rb
+++ app/policies/custom_policy.rb
@@ -1,6 +1,11 @@
 class CustomPolicy < Struct.new(:user, :enable_users)
 
+  def inventory?
+    ['superadmin', 'admin', 'sales_admin', 'sales'].include?(user.role)
+  end
+
+
   def self.custom_methods
-    []
+    ["inventory"]
   end
 end
