900b96de71abd30b809e933984803b9c2e1e9e51
rashneet <rashneet@amuratech.com>
2018-11-30 18:16:58 +0530

    Add hierarchical permissions for channel partners,cp and cp_admin(temp)

--- app/policies/admin/email_policy.rb
+++ app/policies/admin/email_policy.rb
@@ -1,11 +1,30 @@
 class Admin::EmailPolicy < ApplicationPolicy
   class Scope < Scope
+    def self.find_child_ids(user)
+      manager_ids = ids = [user.id]
+      while(User.in(manager_id: manager_ids).count!=0)
+        manager_ids = User.in(manager_id: manager_ids).pluck(:id)
+        ids << manager_ids
+      end
+      ids.flatten!
+    end
+
     def resolve
-      scope.all if ['superadmin', 'admin', 'crm', 'sales_admin', 'sales'].include?(user.role) 
+      if ['superadmin', 'admin', 'crm', 'sales_admin', 'sales'].include?(user.role) 
+        scope.all 
+      elsif ['cp_admin', 'cp', 'channel_partner'].include?(user.role)
+        scope.in(recipient_ids: Scope.find_child_ids(user))
+      end
     end
   end
 
   def show?
-    true
+    if ['superadmin', 'admin', 'crm', 'sales_admin', 'sales'].include?(user.role)
+      true
+    elsif ['cp_admin', 'cp', 'channel_partner'].include?(user.role)
+      !(record.recipient_ids & Scope.find_child_ids(user)).empty?
+    else
+      false
+    end
   end
 end
