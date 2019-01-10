8f6e2a76783c916aca46fb75d67638d224a61d70
rashneet <rashneet@amuratech.com>
2019-01-10 18:28:31 +0530

    Split channel partner policy

---
+++ app/policies/buyer/channel_partner_policy.rb
@@ -0,0 +1,3 @@
+class Buyer::ChannelPartnerPolicy < ChannelPartnerPolicy
+  # def index? def export? def create? def new? def update? def edit? def permitted_attributes from ChannelPartnerPolicy
+end
