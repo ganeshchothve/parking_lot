c61ab04569072625c6de8c9bb5dace0e3472cb64
sanket <sanket.r@amuratech.com>
2019-01-24 18:02:17 +0530

    change UI - For new buyer referral code is auto generated.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,13 +4,13 @@ module UsersHelper
     if current_user.referral_code.blank?
       link_to t('referrals.generate_code.link_name'), generate_code_buyer_referrals_path, method: :post, remote: true, class: 'btn btn-sm btn-default'
     else
-      text_field_tag '', current_user.referral_code, readonly: true, class: 'form-control text-right'
+      text_field_tag '', current_user.referral_code, readonly: true, class: 'form-control'
     end
   end
 
   def invite_friend_link
     if policy([:buyer, :referral]).new?
-      link_to t('referrals.new.link_name'), new_buyer_referral_path, class: 'btn btn-primary modal-remote-form-link pull-left ml-2 btn-sm', data:{ event_category: 'Section', event_action: 'Click', event_name: 'Invite Friend'}
+      link_to t('referrals.new.link_name'), new_buyer_referral_path, class: 'btn btn-primary modal-remote-form-link pull-left', data:{ event_category: 'Section', event_action: 'Click', event_name: 'Invite Friend'}
     else
       ''
     end
