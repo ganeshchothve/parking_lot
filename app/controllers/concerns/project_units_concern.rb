e684c075925a83f3613a34bc28e12290a06fd5bd
sanket <sanket.r@amuratech.com>
2019-02-04 17:48:32 +0530

    Added scheme show page to tower page.

--- app/controllers/concerns/project_units_concern.rb
+++ app/controllers/concerns/project_units_concern.rb
@@ -8,6 +8,24 @@ module ProjectUnitsConcern
     render layout: false
   end
 
+  #
+  # This show action for Admin users where Admin can view details of a particular project unit.
+  #
+  # @return [{}] record with array of Hashes.
+  # GET /admin/project_units/:id
+  #
+  def show
+    @scheme = Scheme.where(_id: params[:selected_scheme_id]).first
+    @project_unit.scheme=(@scheme) if @scheme
+
+    respond_to do |format|
+      format.json { render json: @project_unit }
+      format.html do
+        render layout: params[:layout].blank?, template: 'admin/project_units/show'
+      end
+    end
+  end
+
   private
 
   def set_project_unit
