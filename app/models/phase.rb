fbca5c3867fc2f8780ae01e9cb62d79a205a5b16
aakrutishitut <aakruti@amuratech.com>
2019-02-08 17:05:18 +0530

    Changes

--- app/models/phase.rb
+++ app/models/phase.rb
@@ -2,6 +2,6 @@ class Phase
   include Mongoid::Document
   include Mongoid::Timestamps
   field :name, type: String
-  belongs_to :account
+  belongs_to :account, optional: true
   has_many :project_units
 end
