2be434678dca7e9e7c524c54c3f2eafe75f27b36
aakrutishitut <aakruti@amuratech.com>
2019-02-19 12:59:39 +0530

    partial

--- spec/controllers/booking_detail_scheme_controller_spec.rb
+++ spec/controllers/booking_detail_scheme_controller_spec.rb
@@ -2,11 +2,17 @@ require 'rails_helper'
 RSpec.describe BookingDetailSchemesController, type: :controller do
   describe "updating booking detail scheme" do
     before(:each) do
-      @admin = create(:admin, booking_portal_client: Client.asc(:created_at).first )
-      sign_in_app(@admin)
-      @user = create(:user, booking_portal_client: Client.asc(:created_at).first )
+      client = create(:client)
+      admin = create(:admin)
+      sign_in_app(admin)
+      @user = create(:user)
       kyc = create(:user_kyc, creator_id: @user.id, user: @user)
-      @project_unit = create(:project_unit, booking_portal_client: Client.asc(:created_at).first )
+      @project_unit = create(:project_unit)
+      @project_unit.status = 'hold'
+      @project_unit.user = @user
+      @project_unit.primary_user_kyc_id = kyc.id
+      @project_unit.save
+      @receipt = create(:receipt, user: @user, creator_id: admin.id, project_unit: @project_unit)
     end
 
     it "success response" do
