c3bdd0f7918acb7d9d01ae1881e24dcb5ba7130c
aakrutishitut <aakruti@amuratech.com>
2019-02-20 12:39:08 +0530

    mistake corrected

--- app/controllers/booking_detail_schemes_controller.rb
+++ app/controllers/booking_detail_schemes_controller.rb
@@ -76,7 +76,7 @@ class BookingDetailSchemesController < ApplicationController
   def update
     modify_params
     @scheme.assign_attributes(permitted_attributes(@scheme))
-    @scheme.status = 'under_negotiation' if @scheme.payment_adjustments.last.new_record?
+    @scheme.status = 'under_negotiation' if @scheme.payment_adjustments.present? && @scheme.payment_adjustments.last.new_record?
     @scheme.approved_by = current_user if @scheme.event.present? && @scheme.event == 'approved'
     respond_to do |format|
       if @scheme.save
