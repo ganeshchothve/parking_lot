c063e080fb8122b03ef6582be8f9c60686cddb51
sanket <sanket.r@amuratech.com>
2019-02-25 17:11:54 +0530

    added sidekiq manager

--- config/schedule.rb
+++ config/schedule.rb
@@ -18,17 +18,29 @@
 # end
 
 # Learn more: http://github.com/javan/whenever
+
+every 2.minute, roles: [:app, :staging] do
+  runner "Amura::SidekiqManager.run"
+end
+
+every 8.hour, roles: [:app, :staging] do
+  runner "Amura::SidekiqManager.restart"
+end
+
 every 3.minutes do
   runner "Gamification::Job.new.execute"
 end
+
 every 1.day, at: "4:30 am" do
   runner "ProjectUnitRemindersAndAutoRelease::Job.daily_reminder_for_booking_payment"
   runner "ReceiptCleaner.perform"
   # runner "ProjectUnitRemindersAndAutoRelease::Job.release_project_unit"
 end
+
 every 1.minute do
   runner "UpgradePricing.perform"
 end
+
 every 1.day, at: "3:30 pm" do
   runner "DailySmsReportWorker.new.perform"
 end
