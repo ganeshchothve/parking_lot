7b59bbf2c0091c11918494394eb9b845a9a889c4
rashneet <rashneet@amuratech.com>
2019-03-14 20:03:24 +0530

    Temp commit

--- app/controllers/buyer/user_requests_controller.rb
+++ app/controllers/buyer/user_requests_controller.rb
@@ -30,7 +30,6 @@ class Buyer::UserRequestsController < BuyerController
   def create
     @user_request = associated_class.new(user_id: @user.id, created_by: current_user)
     @user_request.assign_attributes(permitted_user_request_attributes)
-    @user_request.pending! if @user_request.event == 'pending'
     respond_to do |format|
       if @user_request.save
         format.html { redirect_to edit_buyer_user_request_path(@user_request, request_type: @user_request.class.model_name.element), notice: 'Request registered successfully.' }
