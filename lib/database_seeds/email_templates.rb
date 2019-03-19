df9e28f4c88f8ff85b3710014545f3fe9830c029
sanket <sanket.r@amuratech.com>
2019-03-19 13:24:32 +0530

    Update email template seed.

--- lib/database_seeds/email_templates.rb
+++ lib/database_seeds/email_templates.rb
@@ -1,3 +1,4 @@
+# DatabaseSeeds::EmailTemplates.seed(Client.first.id)
 module DatabaseSeeds
   module EmailTemplates
     def self.seed client_id
@@ -7,6 +8,7 @@ module DatabaseSeeds
       DatabaseSeeds::EmailTemplates::Receipt.seed client_id
       DatabaseSeeds::EmailTemplates::UserRequest.seed client_id
       DatabaseSeeds::EmailTemplates::User.seed client_id
+      DatabaseSeeds::EmailTemplates::BookingDetailScheme.seed client_id
 
       Template::EmailTemplate.create!(booking_portal_client_id: client_id, subject_class: "UserKyc", name: "user_kyc_added", subject: "User kyc added <%= self.name %>", content: 'test') if ::Template::EmailTemplate.where(name: "user_kyc_added").blank?
     end
