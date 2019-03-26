5cff1b89e4d6adcc95eeaa74635693bb3f88f92e
rashneet <rashneet@amuratech.com>
2019-03-26 20:12:25 +0530

    Add specs for negative cases and refactor service class

--- spec/controllers/admin/user_requests_swap_controller_spec.rb
+++ spec/controllers/admin/user_requests_swap_controller_spec.rb
@@ -39,6 +39,7 @@ RSpec.describe Admin::UserRequestsController, type: :controller do
           user_request_params = { event: 'rejected', user_id: @user.id }
           patch :update, params: { user_request_swap: user_request_params, request_type: 'swap', id: user_request.id }
           expect(booking_detail.reload.status).to eq('blocked')
+          expect(booking_detail.reload.project_unit.status).to eq('blocked')
           expect(user_request.reload.status).to eq('rejected')
           expect(alternate_project_unit.reload.status).to eq('available')
         end
@@ -46,12 +47,13 @@ RSpec.describe Admin::UserRequestsController, type: :controller do
     end
 
     context 'RESOLVED' do
-      it 'successfully, current project unit is made available, alternate project unit and booking detail is blocked' do
+      it 'successfully, current project unit is made available, current booking detail status changes to swapped, alternate project unit and booking detail status is blocked' do
         booking_detail = book_project_unit(@user)
         booking_detail_scheme = create(:booking_detail_scheme, derived_from_scheme_id: @scheme.id, booking_detail: booking_detail, status: 'approved', project_unit_id: booking_detail.project_unit_id, user_id: booking_detail.user_id)
         alternate_project_unit = create(:project_unit)
         user_request = create(:pending_user_request_swap, project_unit_id: booking_detail.project_unit_id, alternate_project_unit_id: alternate_project_unit.id, user_id: booking_detail.user_id, created_by_id: @admin.id, booking_detail_id: booking_detail.id, event: 'pending')
         user_request_params = { event: 'processing', user_id: @user.id }
+        count = BookingDetailScheme.count
         expect { patch :update, params: { user_request_swap: user_request_params, request_type: 'swap', id: user_request.id } }.to change { BookingDetail.count }.by(1)
         # expect(ProjectUnitCancelWorker.jobs.size).to eq(1)
         expect(user_request.reload.status).to eq('resolved')
@@ -59,110 +61,85 @@ RSpec.describe Admin::UserRequestsController, type: :controller do
         expect(booking_detail.reload.project_unit.status).to eq('available')
         expect(alternate_project_unit.reload.status).to eq('blocked')
         expect(BookingDetail.first.status).to eq('blocked')
+        expect(BookingDetailScheme.count).to eq(count + 1)
+        expect(BookingDetailScheme.last.booking_detail).to eq(BookingDetail.first)
       end
 
-      # it 'when receipt status changes from clearance_pending to cancelled' do
-      #   receipt = create(:receipt, user_id: @user.id, total_amount: 50_000, status: 'clearance_pending')
-      #   booking_detail = book_project_unit(@user, nil, receipt)
-      #   user_request = create(:pending_user_request_cancellation, project_unit_id: booking_detail.project_unit_id, user_id: booking_detail.user_id, created_by_id: @admin.id, booking_detail_id: booking_detail.id, event: 'pending')
-      #   user_request_params = { event: 'processing', user_id: @user.id }
-      #   expect { patch :update, params: { user_request_cancellation: user_request_params, request_type: 'cancellation', id: user_request.id } }.to change { Receipt.count }.by(1)
-      #   # expect(ProjectUnitCancelWorker.jobs.size).to eq(1)
-      #   expect(booking_detail.reload.status).to eq('cancelled')
-      #   expect(%w[available management employee].include?(booking_detail.project_unit.status))
-      #   expect(receipt.reload.status).to eq('cancelled')
-      #   expect(Receipt.last.status).to eq('clearance_pending')
-      #   expect(user_request.reload.status).to eq('resolved')
-      # end
+      context 'successfully, receipt status is ' do
+        it 'changed from success or clearance pending or pending to cancelled ' do
+          booking_detail = book_project_unit(@user)
+          %w[clearance_pending pending].each do |status|
+            booking_detail.receipts << create(:receipt, user_id: @user.id, total_amount: 30_000, status: status)
+          end
+          booking_detail_scheme = create(:booking_detail_scheme, derived_from_scheme_id: @scheme.id, booking_detail: booking_detail, status: 'approved', project_unit_id: booking_detail.project_unit_id, user_id: booking_detail.user_id)
+          alternate_project_unit = create(:project_unit)
+          user_request = create(:pending_user_request_swap, project_unit_id: booking_detail.project_unit_id, alternate_project_unit_id: alternate_project_unit.id, user_id: booking_detail.user_id, created_by_id: @admin.id, booking_detail_id: booking_detail.id, event: 'pending')
+          user_request_params = { event: 'processing', user_id: @user.id }
+          expect { patch :update, params: { user_request_swap: user_request_params, request_type: 'swap', id: user_request.id } }.to change { Receipt.count }.by(3)
+          # expect(ProjectUnitCancelWorker.jobs.size).to eq(1)
+          expect(user_request.reload.status).to eq('resolved')
+          expect(booking_detail.reload.status).to eq('swapped')
+          booking_detail.receipts.each do |receipt|
+            expect(receipt.status).to eq('cancelled')
+          end
+        end
 
-      # it 'when receipt status is pending, project_unit set to nil' do
-      #   receipt = create(:receipt, user_id: @user.id, total_amount: 50_000, status: 'pending')
-      #   booking_detail = book_project_unit(@user, nil, receipt)
-      #   user_request = create(:pending_user_request_cancellation, project_unit_id: booking_detail.project_unit_id, user_id: booking_detail.user_id, created_by_id: @admin.id, booking_detail_id: booking_detail.id, event: 'pending')
-      #   user_request_params = { event: 'processing', user_id: @user.id }
-      #   patch :update, params: { user_request_cancellation: user_request_params, request_type: 'cancellation', id: user_request.id }
-      #   # expect(ProjectUnitCancelWorker.jobs.size).to eq(1)
-      #   expect(booking_detail.reload.status).to eq('cancelled')
-      #   expect(%w[available management employee].include?(booking_detail.project_unit.status))
-      #   expect(receipt.reload.project_unit.present?).to eq(false)
-      #   expect(user_request.reload.status).to eq('resolved')
-      # end
+        it 'the same(failed or available_for_refund or refunded or cancelled)' do
+          booking_detail = book_project_unit(@user)
+          %w[failed available_for_refund refunded cancelled].each do |status|
+            booking_detail.receipts << create(:receipt, user_id: @user.id, total_amount: 30_000, status: status)
+          end
+          booking_detail_scheme = create(:booking_detail_scheme, derived_from_scheme_id: @scheme.id, booking_detail: booking_detail, status: 'approved', project_unit_id: booking_detail.project_unit_id, user_id: booking_detail.user_id)
+          alternate_project_unit = create(:project_unit)
+          user_request = create(:pending_user_request_swap, project_unit_id: booking_detail.project_unit_id, alternate_project_unit_id: alternate_project_unit.id, user_id: booking_detail.user_id, created_by_id: @admin.id, booking_detail_id: booking_detail.id, event: 'pending')
+          user_request_params = { event: 'processing', user_id: @user.id }
+          expect { patch :update, params: { user_request_swap: user_request_params, request_type: 'swap', id: user_request.id } }.to change { Receipt.count }.by(1)
+          # expect(ProjectUnitCancelWorker.jobs.size).to eq(1)
+          expect(user_request.reload.status).to eq('resolved')
+          expect(booking_detail.reload.status).to eq('swapped')
+          booking_detail.receipts.each do |receipt|
+            expect(%w[failed available_for_refund refunded cancelled].include?(receipt.status)).to eq(true)
+          end
+        end
+      end
     end
 
-    # describe 'rejected when processing and processing fails' do
-    #   context 'receipt reverted' do
-    #     it 'when receipt success -> available_for_refund -> success' do
-    #       booking_detail = book_project_unit(@user)
-    #       user_request = create(:pending_user_request_cancellation, project_unit_id: booking_detail.project_unit_id, user_id: booking_detail.user_id, created_by_id: @admin.id, booking_detail_id: booking_detail.id, event: 'pending')
-    #       user_request_params = { event: 'processing', user_id: @user.id }
-    #       Receipt.any_instance.stub(:available_for_refund!).and_return false
-    #       Receipt.any_instance.stub(:errors).and_return(ActiveModel::Errors.new(Receipt.new).tap { |e| e.add(:payment_mode, 'cannot be nil') })
-    #       patch :update, params: { user_request_cancellation: user_request_params, request_type: 'cancellation', id: user_request.id }
-    #       # expect(ProjectUnitCancelWorker.jobs.size).to eq(1)
-    #       expect(booking_detail.receipts.first.reload.status).to eq('success')
-    #       expect(user_request.reload.status).to eq('rejected')
-    #       expect(booking_detail.reload.status).to eq('blocked')
-    #     end
+    context 'REJECTED due to internal error' do
+      it 'creation of new booking detail scheme failed, user_request rejected, booking detail changes to blocked' do
+        booking_detail = book_project_unit(@user)
+        booking_detail_scheme = create(:booking_detail_scheme, derived_from_scheme_id: @scheme.id, booking_detail: booking_detail, status: 'approved', project_unit_id: booking_detail.project_unit_id, user_id: booking_detail.user_id)
+        alternate_project_unit = create(:project_unit)
+        user_request = create(:pending_user_request_swap, project_unit_id: booking_detail.project_unit_id, alternate_project_unit_id: alternate_project_unit.id, user_id: booking_detail.user_id, created_by_id: @admin.id, booking_detail_id: booking_detail.id, event: 'pending')
+        user_request_params = { event: 'processing', user_id: @user.id }
+        BookingDetailScheme.any_instance.stub(:save).and_return false
+        BookingDetailScheme.any_instance.stub(:errors).and_return(ActiveModel::Errors.new(BookingDetailScheme.new).tap { |e| e.add(:project_unit, 'cannot be blank') })
+        count = BookingDetailScheme.count
+        expect { patch :update, params: { user_request_swap: user_request_params, request_type: 'swap', id: user_request.id } }.to change { BookingDetail.count }.by(0)
+        # expect(ProjectUnitCancelWorker.jobs.size).to eq(1)
+        expect(user_request.reload.status).to eq('rejected')
+        expect(booking_detail.reload.status).to eq('blocked')
+        expect(booking_detail.reload.project_unit.status).to eq('blocked')
+        expect(alternate_project_unit.reload.status).to eq('available')
+        expect(BookingDetailScheme.count).to eq(count)
+      end
 
-    #     it 'when receipt clearance_pending -> cancelled -> clearance_pending' do
-    #       receipt = create(:receipt, user_id: @user.id, total_amount: 30_000, status: 'clearance_pending')
-    #       booking_detail = book_project_unit(@user, nil, receipt)
-    #       user_request = create(:pending_user_request_cancellation, project_unit_id: booking_detail.project_unit_id, user_id: booking_detail.user_id, created_by_id: @admin.id, booking_detail_id: booking_detail.id, event: 'pending')
-    #       user_request_params = { event: 'processing', user_id: @user.id }
-    #       count = Receipt.count
-    #       Receipt.any_instance.stub(:cancel!).and_return false
-    #       Receipt.any_instance.stub(:errors).and_return(ActiveModel::Errors.new(Receipt.new).tap { |e| e.add(:payment_mode, 'cannot be nil') })
-    #       patch :update, params: { user_request_cancellation: user_request_params, request_type: 'cancellation', id: user_request.id }
-    #       # expect(ProjectUnitCancelWorker.jobs.size).to eq(1)
-    #       expect(receipt.reload.status).to eq('clearance_pending')
-    #       expect(user_request.reload.status).to eq('rejected')
-    #       expect(booking_detail.reload.status).to eq('blocked')
-    #       expect(Receipt.count).to eq(count)
-    #     end
-
-    #     it 'when receipt clearance_pending, dup receipt fails' do
-    #       receipt = create(:receipt, user_id: @user.id, total_amount: 30_000, status: 'clearance_pending')
-    #       booking_detail = book_project_unit(@user, nil, receipt)
-    #       user_request = create(:pending_user_request_cancellation, project_unit_id: booking_detail.project_unit_id, user_id: booking_detail.user_id, created_by_id: @admin.id, booking_detail_id: booking_detail.id, event: 'pending')
-    #       user_request_params = { event: 'processing', user_id: @user.id }
-    #       count = Receipt.count
-    #       Receipt.any_instance.stub(:cancel!).and_return true
-    #       Receipt.any_instance.stub(:save).and_return false
-    #       Receipt.any_instance.stub(:errors).and_return(ActiveModel::Errors.new(Receipt.new).tap { |e| e.add(:payment_mode, 'cannot be nil') })
-    #       patch :update, params: { user_request_cancellation: user_request_params, request_type: 'cancellation', id: user_request.id }
-    #       expect(receipt.reload.status).to eq('clearance_pending')
-    #       expect(user_request.reload.status).to eq('rejected')
-    #       expect(booking_detail.reload.status).to eq('blocked')
-    #       expect(Receipt.count).to eq(count)
-    #     end
-
-    #     it 'when receipt pending, project unit nil reverted' do
-    #       receipt = create(:receipt, user_id: @user.id, total_amount: 50_000, status: 'pending')
-    #       booking_detail = book_project_unit(@user, nil, receipt)
-    #       user_request = create(:pending_user_request_cancellation, project_unit_id: booking_detail.project_unit_id, user_id: booking_detail.user_id, created_by_id: @admin.id, booking_detail_id: booking_detail.id, event: 'pending')
-    #       user_request_params = { event: 'processing', user_id: @user.id }
-    #       Receipt.any_instance.stub(:save).and_return false
-    #       Receipt.any_instance.stub(:errors).and_return(ActiveModel::Errors.new(Receipt.new).tap { |e| e.add(:payment_mode, 'cannot be nil') })
-    #       patch :update, params: { user_request_cancellation: user_request_params, request_type: 'cancellation', id: user_request.id }
-    #       expect(receipt.reload.status).to eq('pending')
-    #       expect(receipt.project_unit.reload.present?).to eq(true)
-    #       expect(user_request.reload.status).to eq('rejected')
-    #       expect(booking_detail.reload.status).to eq('blocked')
-    #     end
-    #   end
-
-    #   it 'project unit make available failed' do
-    #     booking_detail = book_project_unit(@user)
-    #     user_request = create(:pending_user_request_cancellation, project_unit_id: booking_detail.project_unit_id, user_id: booking_detail.user_id, created_by_id: @admin.id, booking_detail_id: booking_detail.id, event: 'pending')
-    #     user_request_params = { event: 'processing', user_id: @user.id }
-    #     ProjectUnit.any_instance.stub(:save).and_return false
-    #     ProjectUnit.any_instance.stub(:errors).and_return(ActiveModel::Errors.new(ProjectUnit.new).tap { |e| e.add(:name, 'invalid') })
-    #     patch :update, params: { user_request_cancellation: user_request_params, request_type: 'cancellation', id: user_request.id }
-    #     # expect(ProjectUnitCancelWorker.jobs.size).to eq(1)
-    #     expect(booking_detail.reload.status).to eq('blocked')
-    #     expect(booking_detail.project_unit.status).to eq('blocked')
-    #     expect(user_request.reload.status).to eq('rejected')
-    #   end
-    # end
+      it 'making old project unit available failed, user_request rejected, booking detail changes to blocked' do
+        booking_detail = book_project_unit(@user)
+        booking_detail_scheme = create(:booking_detail_scheme, derived_from_scheme_id: @scheme.id, booking_detail: booking_detail, status: 'approved', project_unit_id: booking_detail.project_unit_id, user_id: booking_detail.user_id)
+        alternate_project_unit = create(:project_unit)
+        user_request = create(:pending_user_request_swap, project_unit_id: booking_detail.project_unit_id, alternate_project_unit_id: alternate_project_unit.id, user_id: booking_detail.user_id, created_by_id: @admin.id, booking_detail_id: booking_detail.id, event: 'pending')
+        user_request_params = { event: 'processing', user_id: @user.id }
+        ProjectUnit.any_instance.stub(:save).and_return false
+        ProjectUnit.any_instance.stub(:errors).and_return(ActiveModel::Errors.new(ProjectUnit.new).tap { |e| e.add(:status, 'cannot be blank') })
+        count = BookingDetailScheme.count
+        expect { patch :update, params: { user_request_swap: user_request_params, request_type: 'swap', id: user_request.id } }.to change { BookingDetail.count }.by(0)
+        # expect(ProjectUnitCancelWorker.jobs.size).to eq(1)
+        expect(user_request.reload.status).to eq('rejected')
+        expect(booking_detail.reload.status).to eq('blocked')
+        expect(booking_detail.reload.project_unit.status).to eq('blocked')
+        expect(alternate_project_unit.reload.status).to eq('available')
+        expect(BookingDetailScheme.count).to eq(count)
+      end
+    end
   end
 end
