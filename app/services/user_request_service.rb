505f78c9d0992423ac59e42b581c3a83d2be4fdf
rashneet <rashneet@amuratech.com>
2019-03-27 11:57:11 +0530

    Add user_request service file

---
+++ app/services/user_request_service.rb
@@ -0,0 +1,38 @@
+class UserRequestService
+  attr_accessor :user_request, :user
+
+  def initialize(user_request)
+    @user_request = user_request
+    @user = @user_request.user
+    send_notifications
+  end
+
+  def send_email
+    Email.create!(
+      booking_portal_client_id: user.booking_portal_client_id,
+      email_template_id: Template::EmailTemplate.find_by(name: "#{user_request.class.model_name.element}_request_created").id,
+      recipients: [user],
+      cc_recipients: (user.manager_id.present? ? [user.manager] : []),
+      triggered_by_id: user_request.id,
+      triggered_by_type: user_request.class.to_s
+    )
+  end
+
+  def send_sms
+    template = Template::SmsTemplate.where(name: "#{user_request.class.model_name.element}_request_created").first
+    if template.present?
+      Sms.create!(
+        booking_portal_client_id: user.booking_portal_client_id,
+        recipient_id: user.id,
+        sms_template_id: template.id,
+        triggered_by_id: user_request.id,
+        triggered_by_type: user_request.class.to_s
+      )
+    end
+  end
+
+  def send_notifications
+    send_email if user.booking_portal_client.email_enabled?
+    send_sms if user_request.project_unit.present? && user.booking_portal_client.sms_enabled?
+  end
+end
