6afdad0c3aeb0587456aff3b03e73576d6141a7d
sanket <sanket.r@amuratech.com>
2019-04-19 14:24:25 +0530

    update user request and swapping process

--- app/helpers/user_requests_helper.rb
+++ app/helpers/user_requests_helper.rb
@@ -6,4 +6,13 @@ module UserRequestsHelper
   def get_new_user_request_url(klass, booking_detail)
     current_user.buyer? ? new_buyer_user_request_path(booking_detail_id: booking_detail.id, request_type: klass.model_name.element) : new_admin_user_user_request_path(user_id: booking_detail.user_id, booking_detail_id: booking_detail.id, request_type: klass.model_name.element)
   end
+
+  def user_request_status_badge(user_request)
+    case user_request.status
+    when 'rejected'
+      'danger'
+    when 'pending'
+      'primary'
+    end
+  end
 end
