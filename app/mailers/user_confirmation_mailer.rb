4058773993259c6e976c6fe4b6b64c71dd3678f2
aakrutishitut <aakruti@amuratech.com>
2019-05-02 16:09:18 +0530

    deletions

--- app/mailers/user_confirmation_mailer.rb
+++
@@ -1,7 +0,0 @@
-class UserConfirmationMailer < ApplicationMailer
-  def send_confirmation (user_id, token)
-    @resource = User.find(user_id)
-    @token = token
-    mail(to: @resource.email, subject: "Confirmation instructions")
-  end
-end
