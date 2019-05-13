f96ea8586d6aff0b91b31f42afd94bcf2e3c9abd
sanket <sanket.r@amuratech.com>
2019-05-13 13:19:38 +0530

    fixed user document upload issue

--- app/policies/buyer/asset_policy.rb
+++ app/policies/buyer/asset_policy.rb
@@ -3,6 +3,10 @@ class Buyer::AssetPolicy < AssetPolicy
     "Buyer::#{record.assetable_type}Policy".constantize.new(user, record.assetable).update?
   end
 
+  def update?
+    record.assetable.try(:user_id) == user.id
+  end
+
   def destroy?
     "Buyer::#{record.assetable_type}Policy".constantize.new(user, record.assetable).update?
   end
