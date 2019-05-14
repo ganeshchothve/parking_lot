456e23ab567742ec43736c1319dc3c319032b1f4
aakrutishitut <aakruti@amuratech.com>
2019-05-14 16:36:46 +0530

    change

--- app/models/user_request.rb
+++ app/models/user_request.rb
@@ -27,7 +27,7 @@ class UserRequest
   validates :status, inclusion: { in: STATUS }
   validates :reason_for_failure, presence: true, if: proc { |record| record.rejected? }
   validates :requestable_id, uniqueness: true, if: proc{ |record|
-     (['pending','processing','resolved'] && UserRequest.where(requestable_id: record.requestable_id).pluck(:status).uniq).empty?
+     (['pending','processing','resolved'] & UserRequest.where(requestable_id: record.requestable_id).distinct(:status)).empty?
    }
 
   accepts_nested_attributes_for :notes
