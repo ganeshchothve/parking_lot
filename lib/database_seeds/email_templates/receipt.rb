3591b79dcade423b83c69bdb0577a36468ae3efa
aakrutishitut <aakruti@amuratech.com>
2019-05-15 18:37:11 +0530

    rspecs for receipt state machine

--- lib/database_seeds/email_templates/receipt.rb
+++ lib/database_seeds/email_templates/receipt.rb
@@ -62,6 +62,16 @@ module DatabaseSeeds
           <div class="mt-3"></div>
           <%= current_client.templates.where(_type: "Template::ReceiptTemplate").first.parsed_content(self) %>') if ::Template::EmailTemplate.where(booking_portal_client_id: client_id, name: "receipt_refunded").blank?
 
+        Template::EmailTemplate.create!(booking_portal_client_id: client_id, subject_class: "Receipt", name: "receipt_available_for_refund", subject: "Your payment <%= self.receipt_id %> is available for refund.", content: '<div class="card w-100">
+            <div class="card-body">
+              <p>Dear <%= self.user.name %>,</p>
+              <p>Your refund for payment <%= self.receipt_id %> has been processed.</p>
+              <p>AYour payment <%= self.receipt_id %> is available for refund.</p>
+            </div>
+          </div>
+          <div class="mt-3"></div>
+          <%= current_client.templates.where(_type: "Template::ReceiptTemplate").first.parsed_content(self) %>') if ::Template::EmailTemplate.where(booking_portal_client_id: client_id, name: "receipt_available_for_refund").blank?
+
       end
     end
   end
