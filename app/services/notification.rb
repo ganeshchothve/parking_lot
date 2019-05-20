973315d5d654c2dce7ed6abd61cb239a4bb88067
aakrutishitut <aakruti@amuratech.com>
2019-05-20 23:18:50 +0530

    receipt pdf attached to mail

--- app/services/notification.rb
+++ app/services/notification.rb
@@ -26,7 +26,16 @@ module Notification
       fail "#{keys} required to send notification" if keys.present?
 
       template_name = params.delete "template_name"
-
+      receipt = ::Receipt.find params[:triggered_by_id]
+      if %w[clearance_pending success].include?(receipt.status)
+        pdf = WickedPdf.new.pdf_from_string(Template::ReceiptTemplate.first.parsed_content(receipt))
+        File.open("#{Rails.root}/tmp/receipt_details_#{receipt.receipt_id}.pdf", "wb") do |file|
+          file << pdf
+        end
+        attachments_attributes = []
+        attachments_attributes << {file: File.open("#{Rails.root}/tmp/receipt_details_#{receipt.receipt_id}.pdf")}
+        params[:attachments_attributes] = attachments_attributes
+      end
       ::Email.create!(params.merge(email_template_id: Template::EmailTemplate.where(booking_portal_client_id: params[:booking_portal_client_id]).find_by(name: template_name).id))
     end
   end
