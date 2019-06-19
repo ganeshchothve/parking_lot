c34a1aac972a821e66423fdb4d82fc4db18b2414
sanket <sanket.r@amuratech.com>
2019-06-19 17:27:41 +0530

    fixed sync issue

--- app/models/concerns/sync_details.rb
+++ app/models/concerns/sync_details.rb
@@ -30,12 +30,10 @@ module SyncDetails
     end
 
     def new_details
-      if current_client.selldo_client_id.blank? && current_client.selldo_form_id.blank?
-        sync_log = SyncLog.new
-        @erp_models = ErpModel.where(resource_class: self.class, action_name: 'create', is_active: true)
-        @erp_models.each do |erp|
-          sync_log.sync(erp, self)
-        end
+      sync_log = SyncLog.new
+      _erp_models = ErpModel.where(resource_class: self.class, action_name: 'create', is_active: true)
+      _erp_models.each do |erp|
+        sync_log.sync(erp, self)
       end
     end
 
