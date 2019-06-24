af742c728b39bd851c87148eaac2489b6461f22e
aakrutishitut <aakruti@amuratech.com>
2019-06-24 19:38:51 +0530

    requested changes

--- app/controllers/admin/clients_controller.rb
+++ app/controllers/admin/clients_controller.rb
@@ -19,7 +19,7 @@ class Admin::ClientsController < AdminController
           :type => @client.brochure.content_type,
           :disposition => 'attachment',
           :url_based_filename => true)
-    current_user.portal_stage << PortalStage.new(stage: 'project_info') if current_user.buyer?
+    current_user.portal_stage << PortalStage.new(stage: 'project_info') if current_user.buyer? && current_user.receipts.count == 0
   end
 
   def update
