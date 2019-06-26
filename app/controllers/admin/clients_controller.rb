125fc796f61ca753af8fdd50f97e2756f5e79c10
aakrutishitut <aakruti@amuratech.com>
2019-06-26 19:05:44 +0530

    change for pdf upload

--- app/controllers/admin/clients_controller.rb
+++ app/controllers/admin/clients_controller.rb
@@ -14,8 +14,8 @@ class Admin::ClientsController < AdminController
   # GET /admin/clients/download_brochure
   #
   def download_brochure
-    send_file(@client.brochure.path,
-          :filename => @client.brochure.url,
+    send_file(open(@client.brochure.url),
+          :filename => "Brochure.#{@client.brochure.url.file.extenstion}",
           :type => @client.brochure.content_type,
           :disposition => 'attachment',
           :url_based_filename => true)
