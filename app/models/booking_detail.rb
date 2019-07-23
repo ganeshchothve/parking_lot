fe457f3b94a1a451862f38179370776ef54a9d30
aakrutishitut <aakruti@amuratech.com>
2019-07-23 17:58:24 +0530

    filter booking_detail with name problem fixed

--- app/models/booking_detail.rb
+++ app/models/booking_detail.rb
@@ -65,6 +65,7 @@ class BookingDetail
   scope :filter_by_project_tower_id, ->(project_tower_id) { where(project_unit_id: { "$in": ProjectUnit.where(project_tower_id: project_tower_id).pluck(:_id) })}
   scope :filter_by_user_id, ->(user_id) { where(user_id: user_id)  }
   scope :filter_by_manager_id, ->(manager_id){ where(user_id: { '$in' => User.buyers.where(manager_id: manager_id).distinct(:_id) } ) }
+  scope :filter_by_search, ->(search) { regex = ::Regexp.new(::Regexp.escape(search), 'i'); where(name: regex ) }
   default_scope -> {desc(:created_at)}
 
 
