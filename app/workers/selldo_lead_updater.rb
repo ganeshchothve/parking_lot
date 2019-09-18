76c00454f04ee982c484b8ed046474825254e5e9
Milan Patel <milan@amuratech.com>
2019-09-18 19:04:34 +0530

    Send token_number to sell.do as an array upon receipt generation

--- app/workers/selldo_lead_updater.rb
+++ app/workers/selldo_lead_updater.rb
@@ -3,7 +3,7 @@ class SelldoLeadUpdater
   include Sidekiq::Worker
   include ApplicationHelper
 
-  def perform(user_id, st=nil)
+  def perform(user_id, st=nil, options={})
     return false if Rails.env.test?
     user = User.where(id: user_id).first
     return false unless user
@@ -11,18 +11,22 @@ class SelldoLeadUpdater
     if st.present? && priority[st].present?
       if user.portal_stages.empty?
         user.portal_stages << PortalStage.new(stage: st, priority: priority[st])
-        sell_do(user, st)
       elsif user.portal_stage.priority.to_i <= priority[st].to_i
         user.portal_stages.where(stage:  st).present? ? user.portal_stages.where(stage:  st).first.set(updated_at: Time.now, priority: priority[st]) : user.portal_stages << PortalStage.new(stage: st, priority: priority[st])
-        sell_do(user, st)
       end
+      if st == 'payment_done' && options["token_number"].present?
+        token_numbers = user.receipts.where('$or' => [{ status: { '$in': %w(success clearance_pending) } }, { payment_mode: {'$ne': 'online'}, status: {'$in': %w(pending clearance_pending success)} }]).distinct(:token_number)
+      end
+      sell_do(user, st, token_numbers)
     else
       user.portal_stage
     end
   end
 
-  def sell_do (user, stage)
+  def sell_do (user, stage, token_numbers)
     score = 10
+    custom_hash = { portal_stage: stage }
+    custom_hash[:token_number] = token_numbers if token_numbers.present?
     MixpanelPusherWorker.perform_async(user.mixpanel_id, stage, {}) if current_client.mixpanel_token.present?
     if current_client.selldo_api_key.present?
       if user.buyer? && stage.present? && user.lead_id.present?
@@ -30,9 +34,7 @@ class SelldoLeadUpdater
           lead_id: user.lead_id,
           mixpanel_id: (user.mixpanel_id.present? && user.mixpanel_id != "undefined" && user.mixpanel_id != "null") ? user.mixpanel_id : nil,
           score: score,
-          custom_data: {
-            portal_stage: stage
-          },
+          custom_data: custom_hash,
           api_key: current_client.selldo_api_key
         }
         RestClient.post("https://app.sell.do/api/leads/create", params)
