e8c8a510ab7b56c2462489a509eb9ba7ba0d59bd
Milan Patel <milan@amuratech.com>
2019-09-20 14:02:13 +0530

    Fixed issues - allowed bookings on user set only if  allowed bookings are not present - hash displayed from en.yml in time slot options

--- app/observers/user_observer.rb
+++ app/observers/user_observer.rb
@@ -2,7 +2,7 @@ class UserObserver < Mongoid::Observer
   include ApplicationHelper
 
   def before_validation user
-    user.allowed_bookings = current_client.allowed_bookings_per_user
+    user.allowed_bookings ||= current_client.allowed_bookings_per_user
     user.booking_portal_client_id ||= current_client.id
   end
 
