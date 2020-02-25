e1484e408b1225eda7ff4b846277ac9da5094695
Milan Patel <milan@amuratech.com>
2020-02-25 14:54:47 +0530

    Separate out s3 buckets for public & private assets

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,14 +3,14 @@ if Rails.env.production? || Rails.env.staging?
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider:              'AWS',
-      aws_access_key_id:     ENV_CONFIG[:asset_sync]['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV_CONFIG[:asset_sync]['AWS_SECRET_ACCESS_KEY'],
+      aws_access_key_id:     ENV_CONFIG[:carrierwave]['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV_CONFIG[:carrierwave]['AWS_SECRET_ACCESS_KEY'],
       use_iam_profile:       true,
-      region:                ENV_CONFIG[:asset_sync]['FOG_REGION']
+      region:                ENV_CONFIG[:carrierwave]['FOG_REGION']
     }
-    config.fog_directory  = ENV_CONFIG[:asset_sync]['FOG_DIRECTORY']
-    config.fog_public     = true
-    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
+    config.fog_directory  = ENV_CONFIG[:carrierwave]['FOG_DIRECTORY']
+    config.fog_public     = false
+    #config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
   end
 else
   CarrierWave.configure do |config|
