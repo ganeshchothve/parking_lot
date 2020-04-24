4f73d09b68fe3dbf7f0d9782346c05889dd5cbe9
Milan Patel <milan@amuratech.com>
2020-04-25 02:54:36 +0530

    Put env config file for production under config folder

--- config/application.rb
+++ config/application.rb
@@ -12,11 +12,7 @@ require "action_cable/engine"
 require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
-if Rails.env == "production"
-  ENV_CONFIG = (YAML.load(File.open( "/usr/local/generic-booking-portal-env.yml" ).read).symbolize_keys).with_indifferent_access
-else
-  ENV_CONFIG = (YAML.load(File.open( "config/generic-booking-portal-env.yml" ).read).symbolize_keys).with_indifferent_access
-end
+ENV_CONFIG = (YAML.load(File.open( "config/generic-booking-portal-env.yml" ).read).symbolize_keys).with_indifferent_access
 
 DEVISE_ORM = :mongoid
 
