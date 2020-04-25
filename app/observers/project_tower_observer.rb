ffa52a6426ba83c05f641183e097e35fc97460f8
Milan Patel <milan@amuratech.com>
2020-04-25 14:20:58 +0530

    Changes for inventory upload

--- app/observers/project_tower_observer.rb
+++ app/observers/project_tower_observer.rb
@@ -3,6 +3,7 @@ class ProjectTowerObserver < Mongoid::Observer
     project = project_tower.project
     cost_sheet_template = Template::CostSheetTemplate.where(booking_portal_client_id: project.booking_portal_client_id, default: true).first
     payment_schedule_template = Template::PaymentScheduleTemplate.where(booking_portal_client_id: project.booking_portal_client_id, default: true).first
-    Scheme.create!(name: "Default #{project_tower.name} scheme", cost_sheet_template_id: cost_sheet_template.id, payment_schedule_template_id: payment_schedule_template.id, project_id: project_tower.project_id, project_tower_id: project_tower.id, default: true, created_by: User.where(role: "admin").first, booking_portal_client_id: project_tower.project.booking_portal_client_id, status: "approved", approved_by: User.where(role: "admin").first, project: project)
+    user = User.where(role: "admin").first || User.where(role: 'superadmin').first
+    Scheme.create!(name: "Default #{project_tower.name} scheme", cost_sheet_template_id: cost_sheet_template.id, payment_schedule_template_id: payment_schedule_template.id, project_id: project_tower.project_id, project_tower_id: project_tower.id, default: true, created_by: user, booking_portal_client_id: project_tower.project.booking_portal_client_id, status: "approved", approved_by: user, project: project)
   end
 end
