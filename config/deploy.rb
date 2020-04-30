3a714b9b0f84a1224f435e5e2c2b51a49d5dfad5
Milan Patel <milan@amuratech.com>
2020-04-30 20:25:39 +0530

    Changes related to Deployment. - Add deploy task for uploading config files, change folder permissions,  & optionally seed db. - Remove old booking-portal-env.yml file - Added example yml config files to copy from

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,3 +41,48 @@ set :linked_dirs, %w{log tmp vendor/bundle public/uploads exports}
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
 set :passenger_restart_with_touch, true
+
+namespace :deploy do
+  desc 'Seeds database'
+  task :seed do
+    on roles(:app) do |server|
+      ask(:client_name, nil)
+      ask(:project_name, nil)
+      ask(:rera_no, nil)
+      set :booking_portal_domain, `ssh -G #{server.hostname} | awk '/^hostname / { print $2 }'`.strip
+      within release_path do
+        execute :bundle, :exec, :"rake db:seed RAILS_ENV=#{fetch(:rails_env)} client_name='#{fetch(:client_name)}' project_name='#{fetch(:project_name)}' rera_no='#{fetch(:rera_no)}' booking_portal_domains='#{fetch(:booking_portal_domain)}'"
+      end
+    end
+  end
+
+  desc 'Uploads required config files'
+  task :upload_configs do
+    on roles(:all) do
+      unless test("[ -f #{deploy_to}/shared/config/lead_conflicts_executers.yml ]")
+        upload!(File.expand_path('../lead_conflicts_executers.yml.example', __FILE__), "#{deploy_to}/shared/config/lead_conflicts_executers.yml")
+      end
+      unless test("[ -f #{deploy_to}/shared/config/sidekiq_manager.yml ]")
+        upload!(File.expand_path('../sidekiq_manager.yml.example', __FILE__), "#{deploy_to}/shared/config/sidekiq_manager.yml")
+      end
+      unless test("[ -f #{deploy_to}/shared/config/generic-booking-portal-env.yml ]")
+        upload!(File.expand_path("../deploy/#{fetch(:application)}-booking-portal-env.yml", __FILE__), "#{deploy_to}/shared/config/generic-booking-portal-env.yml")
+      end
+    end
+  end
+
+  desc 'Change folder permissions'
+  task :change_permissions do
+    on roles(:app) do
+      within release_path do
+        if test("[ $(stat -c '%a' \"#{deploy_to}/shared/exports\" \"#{deploy_to}/shared/tmp\" \"#{deploy_to}/shared/log\" \"#{deploy_to}/shared/public/uploads\" | tr -d '\\n') != \"777777777777\" ]")
+          execute :chmod, '-R', '0777', "#{deploy_to}/shared/exports", "#{deploy_to}/shared/tmp", "#{deploy_to}/shared/log", "#{deploy_to}/shared/public/uploads"
+        end
+      end
+    end
+  end
+
+  before 'deploy:check:linked_files', 'deploy:upload_configs'
+  after :finished, 'deploy:change_permissions'
+  #after :finished, 'deploy:seed'
+end
