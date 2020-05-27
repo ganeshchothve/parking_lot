22d3bcf5f874f97f79b1aea531733b65405f1c12
rashneet <rashneet@amuratech.com>
2020-05-27 22:17:46 +0530

    [IRIS-656]Move code from show booking to erb template for user

--- lib/database_seeds/ui_templates.rb
+++ lib/database_seeds/ui_templates.rb
@@ -4,6 +4,7 @@
 module DatabaseSeeds
   module UITemplate
     def self.seed(client_id)
+      # To render card on admin dashboard
       # if Template::UITemplate.where(name: 'users/_welcome').blank?
       #   Template::UITemplate.create({ booking_portal_client_id: client_id, subject_class: 'View', name: 'users_pwelcome', content: '
       #     <div class="box-content">
@@ -71,23 +72,27 @@ module DatabaseSeeds
       #     </div>' })
       # end
 
+      # To change text above login page for admin/sales/user
       if Template::UITemplate.where(name: 'devise/sessions/new').blank?
         Template::UITemplate.create({ booking_portal_client_id: client_id, subject_class: 'View', name: 'devise/sessions/new', content: '<h1 class="mt-0 fn-24">Welcome to <%= current_project.name %></h1>
           <p class="p-style mt-0">Now is the best time to turn your dream into reality</p>
           <p class="p-style"><strong>Login</strong> to Book an Apartment online</p>' })
       end
 
+      # To change text on register page for user
       if Template::UITemplate.where(name: 'home/register').blank?
         Template::UITemplate.create({ booking_portal_client_id: client_id, subject_class: 'View', name: 'home/register', content: '<h1 class="mt-0 fn-20">Biggest real estate opportunity in Pune<br> Introducing exclusive <%= ProjectUnit.distinct(:unit_configuration_name).map {|x| x.match(/\d*.*\d/).to_s}.uniq.sort.first(3).to_sentence(last_word_connector: " & ") %> Bed residences starting from <%= number_to_indian_currency(DashboardDataProvider.minimum_agreement_price.to_s.match(/(\d+)\d{5}/).try(:captures).try(:first)) %> lakhs</h1>
         <p class="p-style">Home buying can’t get better than this</p>
         <p><strong>Register Now</strong> to Book Online</p>' })
       end
 
+      # To change text on register page for channel_partner
       if Template::UITemplate.where(name: 'channel_partner/new').blank?
         Template::UITemplate.create({ booking_portal_client_id: client_id, subject_class: 'View', name: 'channel_partner/new', content: '<h1 class="mt-0 fn-20">We are a big family of 400+ esteemed partners and we are happy to onboard you</h1>
         <p><strong>Register now</strong> & join our network to explore new opportunities</p>' })
       end
 
+      # To change text in the footer
       if Template::UITemplate.where(name: 'layouts/_navbar_footer').blank?
         Template::UITemplate.create({ booking_portal_client_id: client_id, subject_class: 'View', name: 'layouts/_navbar_footer', content: '
         <div class="container">
@@ -125,8 +130,23 @@ module DatabaseSeeds
         </div>' })
       end
 
+      # To change text above timer when booking a unit
       if Template::UITemplate.where(name: 'searches/checkout').blank?
         Template::UITemplate.create({ booking_portal_client_id: client_id, subject_class: 'View', name: 'searches/checkout', content: '<p class="p-style white"> To be fair to other user interested in this apartments, we have held this unit for some time. Please go through the costs and payments schedule before you make a payment of <%= number_to_indian_currency(@project_unit.blocking_amount || current_client.blocking_amount) %></p>' })
+      end
+
+      # To change text on show booking detail for user
+      # @bd is booking_detail
+      if Template::UITemplate.where(name: 'booking_details/_details').blank?
+        Template::UITemplate.create({ booking_portal_client_id: client_id, subject_class: 'View', name: 'booking_details/_details', content: '<ul>
+            <li>You need to pay <%= number_to_indian_currency(@bd.project_unit.booking_price) %> to confirm the booking</li>
+            <% unless current_client.cancellation_amount.zero? %>
+              <li>Cancellation charges are <%= number_to_indian_currency(current_client.cancellation_amount) %></li>
+            <% end %>
+            <% if @bd.project_unit.auto_release_on %>
+              <li>You have <%= (@bd.project_unit.auto_release_on - Date.today).to_i %> days (before <%= I18n.l(@bd.project_unit.auto_release_on) %>) remaining to confirm this booking. Please make a remaining payment of <%= number_to_indian_currency(@bd.pending_balance.round) %></li>
+            <% end %>
+          </ul>' })
       end
 
       Template::UITemplate.where(booking_portal_client_id: client_id).count
