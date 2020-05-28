d499863f0ddfda1a6898e61b5f34aff3ba557797
rashneet <rashneet@amuratech.com>
2020-05-28 14:11:58 +0530

    [IRIS-676]Move channel partner dashboard code to erb template

--- lib/database_seeds/ui_templates.rb
+++ lib/database_seeds/ui_templates.rb
@@ -7,47 +7,82 @@ module DatabaseSeeds
       # To render card on user dashboard
       if Template::UITemplate.where(name: 'users/_welcome').blank?
         Template::UITemplate.create({ booking_portal_client_id: client_id, subject_class: 'View', name: 'users/_welcome', content: '
-        <h1 class="wc-title white text-center">Welcome <%= current_user.name %></h1>
-        <% if current_user.receipts.present? && !current_client.enable_actual_inventory?(current_user) %>
-          <p class="white text-center fn-300 fn-18">Congratulations on generating your Priority Token for allotment day. Make additional payment to get more tokens</p>
-        <% else %>
-          <p class="white text-center fn-300 fn-18">Now get privileged access to <%= current_project.project_size %> spread township and book your dream home by just paying <%= number_to_indian_currency(current_client.blocking_amount) %><br>400+ customers have already paid a token amount and now it\'s your chance to roll the dice by following below steps</p>
-        <% end %>
-        <ul class="step-booking">
-          <li>
-            <span><%= image_tag "file-invoice.svg", alt: "Invoice" %></span>
-            <%= link_to "Fill KYC Form", [:new, current_user_role_group, :user_kyc], class: "modal-remote-form-link" %>
-          </li>
-          <li>
-            <span><%= image_tag "rupee-blue.svg", alt: "Rupee" %></span>
-            <%= link_to_if !current_user.user_kyc_ids.blank?, "Pay Remaining Amount",[:new, current_user_role_group, :receipt ], { class: "modal-remote-form-link"} %>
-          </li>
-          <li>
-            <span><%= image_tag "get-token.svg", alt: "Building", style: "width:40px;" %></span>
-            <%= link_to_if policy([current_user_role_group, ProjectUnit.new(user: current_user, status: "available")]).hold?, "Get Priority Token", new_search_path %>
-          </li>
-        </ul>
-
-        <% if current_user.kyc_ready? && current_user.booking_details.in(status: BookingDetail::BOOKING_STAGES).count >= current_user.allowed_bookings %>
-            <p class="white text-center fn-14 fn-500">You cannot book any more apartments as you have reached the maximum bookings allowed per customer.</p>
-        <% elsif current_user.kyc_ready? && current_client.enable_actual_inventory?(current_user) %>
-          <%= booking_detail = current_user.booking_details.hold.first %>
-          <% if booking_detail && booking_detail.search %>
-            <!-- <p class="white text-center fn-14 fn-500">You already have a unit on hold.</p> -->
-            <%= link_to "Checkout using unit already held", checkout_user_search_path(booking_detail.search), class: "large-btn black-bg display-block fn-500 center-block show-kyc width-250" %>
-          <% else %>
-            <% if policy([current_user_role_group, ProjectUnit.new(user: current_user, status: "available")]).hold? %>
-              <p class="white text-center fn-14 fn-500">Choose Apartment</p>
-              <%= link_to t("controller.buyer.dashboard.book_apartment"), new_search_path, class: "large-btn black-bg display-block fn-500 center-block show-kyc" %>
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 bg-gradient">
+        <div class="box-content">
+            <h1 class="wc-title white text-center">Welcome <%= current_user.name %></h1>
+            <% if current_user.receipts.present? && !current_client.enable_actual_inventory?(current_user) %>
+              <p class="white text-center fn-300 fn-18">Congratulations on generating your Priority Token for allotment day. Make additional payment to get more tokens</p>
+            <% else %>
+              <p class="white text-center fn-300 fn-18">Now get privileged access to <%= current_project.project_size %> spread township and book your dream home by just paying <%= number_to_indian_currency(current_client.blocking_amount) %><br>400+ customers have already paid a token amount and now it\'s your chance to roll the dice by following below steps</p>
             <% end %>
-          <% end %>
-        <% elsif current_user.kyc_ready? %>
-          <!-- <p class="white text-center fn-14 fn-500">Make Payment to proceed further</p> -->
-          <%= link_to t("controller.buyer.dashboard.add_payment"), new_buyer_receipt_path, class: "large-btn black-bg display-block fn-500 center-block show-kyc modal-remote-form-link" %>
-        <% else %>
-          <!-- <p class="white text-center fn-14 fn-500">Fill your KYC form to proceed further</p> -->
-          <%= link_to t("controller.buyer.dashboard.fill_kyc_form"), new_buyer_user_kyc_path, class: "large-btn black-bg display-block fn-500 center-block show-kyc modal-remote-form-link" %>
-        <% end%>' })
+            <ul class="step-booking">
+              <li>
+                <span><%= image_tag "file-invoice.svg", alt: "Invoice" %></span>
+                <%= link_to "Fill KYC Form", [:new, current_user_role_group, :user_kyc], class: "modal-remote-form-link" %>
+              </li>
+              <li>
+                <span><%= image_tag "rupee-blue.svg", alt: "Rupee" %></span>
+                <%= link_to_if !current_user.user_kyc_ids.blank?, "Pay Remaining Amount",[:new, current_user_role_group, :receipt ], { class: "modal-remote-form-link"} %>
+              </li>
+              <li>
+                <span><%= image_tag "get-token.svg", alt: "Building", style: "width:40px;" %></span>
+                <%= link_to_if policy([current_user_role_group, ProjectUnit.new(user: current_user, status: "available")]).hold?, "Get Priority Token", new_search_path %>
+              </li>
+            </ul>
+
+            <% if current_user.kyc_ready? && current_user.booking_details.in(status: BookingDetail::BOOKING_STAGES).count >= current_user.allowed_bookings %>
+                <p class="white text-center fn-14 fn-500">You cannot book any more apartments as you have reached the maximum bookings allowed per customer.</p>
+            <% elsif current_user.kyc_ready? && current_client.enable_actual_inventory?(current_user) %>
+              <%= booking_detail = current_user.booking_details.hold.first %>
+              <% if booking_detail && booking_detail.search %>
+                <!-- <p class="white text-center fn-14 fn-500">You already have a unit on hold.</p> -->
+                <%= link_to "Checkout using unit already held", checkout_user_search_path(booking_detail.search), class: "large-btn black-bg display-block fn-500 center-block show-kyc width-250" %>
+              <% else %>
+                <% if policy([current_user_role_group, ProjectUnit.new(user: current_user, status: "available")]).hold? %>
+                  <p class="white text-center fn-14 fn-500">Choose Apartment</p>
+                  <%= link_to t("controller.buyer.dashboard.book_apartment"), new_search_path, class: "large-btn black-bg display-block fn-500 center-block show-kyc" %>
+                <% end %>
+              <% end %>
+            <% elsif current_user.kyc_ready? %>
+              <!-- <p class="white text-center fn-14 fn-500">Make Payment to proceed further</p> -->
+              <%= link_to t("controller.buyer.dashboard.add_payment"), new_buyer_receipt_path, class: "large-btn black-bg display-block fn-500 center-block show-kyc modal-remote-form-link" %>
+            <% else %>
+              <!-- <p class="white text-center fn-14 fn-500">Fill your KYC form to proceed further</p> -->
+              <%= link_to t("controller.buyer.dashboard.fill_kyc_form"), new_buyer_user_kyc_path, class: "large-btn black-bg display-block fn-500 center-block show-kyc modal-remote-form-link" %>
+            <% end%>
+          </div>
+        </div>' })
+      end
+
+      # To render card on user dashboard
+      if Template::UITemplate.where(name: 'index/_channel_partner').blank?
+        Template::UITemplate.create({ booking_portal_client_id: client_id, subject_class: 'View', name: 'index/_channel_partner', content: '
+        <div class="col-lg-12 col-xs-12 bg-gradient-cd br-rd-8 col-md-12 col-sm-12">
+          <img src="<%= asset_path \'quality-tag.png\' %>" alt="user icon" class="quality-tag">
+          <div class="row">
+            <div class="col-lg-8 col-md-12 col-sm-12 col-xs-12 offset-lg-2">
+              <div class="box-content">
+                <h1 class="wc-title white text-center">Welcome <%= current_user.name %></h1>
+                <p class="white text-center fn-300 fn-18">Follow these quick steps and get your customers to be one of the privileged few to own a home at <%= current_project.name %></p>
+                <ul class="step-booking">
+                  <li class="light-blue">
+                    <span>
+                      <%= image_tag "file-invoice-lightblue.svg", alt: "Add customer" %>
+                    </span>Add Customers
+                  </li>
+                  <li class="light-blue">
+                    <span><%= image_tag "rupee-lightblue.svg" %></span>Pay Remaining Amount
+                  </li>
+                  <li class="light-blue">
+                    <span><%= image_tag "building-lightblue.svg" %></span><%= BookingDetail.model_name.human %>
+                  </li>
+                </ul>
+                <!-- <p class="white text-center fn-14 fn-500">Fill your KYC form for proceed further</p> -->
+                <%= link_to "Add Customer", new_admin_user_path(role: "user"), class: "large-btn black-bg display-block fn-500 center-block show-kyc modal-remote-form-link" %>
+              </div>
+            </div>
+          </div>
+        </div>' })
       end
 
       # To change text above login page for admin/sales/user
