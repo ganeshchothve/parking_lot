14c32d3927789f32c484a65a0fad8ff470831fd0
rashneet <rashneet@amuratech.com>
2020-06-10 22:56:18 +0530

    [IRIS-704]Add code to make required changes in templates

--- app/observers/client_observer.rb
+++ app/observers/client_observer.rb
@@ -14,6 +14,9 @@ class ClientObserver < Mongoid::Observer
     DatabaseSeeds::SmsTemplate.seed client.id.to_s
     DatabaseSeeds::EmailTemplates.seed client.id.to_s
     DatabaseSeeds::UITemplate.seed client.id.to_s
+    # Email and Sms Templates are disabled by default
+    Template.in(_type: ['Template::SmsTemplate', 'Template::EmailTemplate']).each { |t| t.set(is_active: false) }
+
     Template::CostSheetTemplate.create(name: "Default Cost sheet template", content: Template::CostSheetTemplate.default_content, booking_portal_client_id: client.id, default: true)
     Template::PaymentScheduleTemplate.create(name: "Default payment schedule template", content: Template::PaymentScheduleTemplate.default_content, booking_portal_client_id: client.id, default: true)
     Template::ReceiptTemplate.create(content: Template::ReceiptTemplate.default_content, booking_portal_client_id: client.id)
