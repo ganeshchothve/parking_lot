c611ce169d459ef77f112c373b9eff1f66b26331
aakrutishitut <aakruti@amuratech.com>
2020-06-26 16:26:28 +0530

    Razorpay Routes Integration

--- app/services/payment_gateway_service/razorpay.rb
+++ app/services/payment_gateway_service/razorpay.rb
@@ -10,6 +10,7 @@ module PaymentGatewayService
         @receipt.payment_identifier = params[:payment_id]
         @receipt.tracking_id = params[:payment_id]
         if response.status == 'captured'
+          initiate_transfer_on_razorpay(@receipt, params)
           @receipt.clearance_pending! 
           @receipt.status_message = "success"
         else
@@ -21,6 +22,17 @@ module PaymentGatewayService
         @receipt.status_message = e.to_s
       end
       @receipt.save
+    end
+
+    def initiate_transfer_on_razorpay(receipt, params)
+      account = receipt.account
+      if account.present? && params[:payment_id].present?
+        auth = {:username => account.key, :password => account.secret}
+        response = HTTParty.post("https://api.razorpay.com/v1/payments/#{params[:payment_id]}/transfers",:basic_auth => auth, :body => { :transfers => [{:account => account.account_number.to_s, :amount => (receipt.total_amount * 100), :currency => "INR", :on_hold => false}]}.to_json, :headers => { 'Content-Type' => 'application/json' })
+        response["items"].each do |item|
+          receipt.transfer_details << {id: item.dig("id"), amount: item.dig("amount")}
+        end
+      end
     end
   end
 end
