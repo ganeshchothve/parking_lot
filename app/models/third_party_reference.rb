5b3db20dd6302bd21496bbface5ffb08021f7599
aakrutishitut <aakruti@amuratech.com>
2020-07-28 16:41:22 +0530

    PR change

--- app/models/third_party_reference.rb
+++ app/models/third_party_reference.rb
@@ -8,11 +8,4 @@ class ThirdPartyReference
   embedded_in :reference_model, polymorphic: true
   belongs_to :crm, class_name: 'Crm::Base'
   validates_uniqueness_of :crm_id, scope: :reference_model
-  validate :reference_id_uniqueness
-
-  def reference_id_uniqueness
-    if self.reference_id.present? && reference_model.class.where("third_party_references.reference_id": self.reference_id).present?
-      reference_model.errors.add :base, "Duplicate reference id"
-    end
-  end
 end
