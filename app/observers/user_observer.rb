d19878630a2f9e1bf95996ac582b75b991738bfa
Milan Patel <milan.patel@sell.do>
2020-09-15 23:10:24 +0530

    Moved updating lead_id on user in third party reference from before_save to after_save, to avoid duplicate mongo id problem as it was saving the record twice on create

--- app/observers/user_observer.rb
+++ app/observers/user_observer.rb
@@ -38,6 +38,9 @@ class UserObserver < Mongoid::Observer
     unless user.authentication_token?
       user.reset_authentication_token!
     end
+  end
+
+  def after_save user
     if user.lead_id.present? && crm = Crm::Base.where(domain: ENV_CONFIG.dig(:selldo, :base_url)).first
       user.update_reference_id(user.lead_id, crm.id)
     end
