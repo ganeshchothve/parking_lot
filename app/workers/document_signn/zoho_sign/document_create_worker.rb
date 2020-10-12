3e719b60e21ec575e31cf903c337812567b7839f
aakrutishitut <aakruti@amuratech.com>
2020-10-12 09:55:46 +0530

    minor change

--- app/workers/document_signn/zoho_sign/document_create_worker.rb
+++ app/workers/document_signn/zoho_sign/document_create_worker.rb
@@ -6,8 +6,9 @@ module DocumentSignn
       def perform(document_sign_id, asset_id, options)
         document_sign = DocumentSign.where(id: document_sign_id).first
         asset = Asset.where(id: asset_id).first
+        options = options.with_indifferent_access
         if asset && document_sign && (["request_name", "recipient_name", "recipient_email"] - options.keys).empty?
-          Zoho::Sign.create_and_sign(asset, document_sign, options)
+          ::Zoho::Sign.create_and_sign(asset, document_sign, options)
         else
           Rails.logger.error "Required params not sent"
         end
