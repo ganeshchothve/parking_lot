fc8024b1e03ef057e06b180921bb2c2e0ec42d44
rashneet <rashneet@amuratech.com>
2020-11-02 18:06:39 +0530

    [IRIS-1038] Add code to activate account

--- app/policies/buyer/user_policy.rb
+++ app/policies/buyer/user_policy.rb
@@ -1,5 +1,5 @@
 class Buyer::UserPolicy < UserPolicy
-  # def index? def resend_confirmation_instructions? def resend_password_instructions? def export? def confirm_via_otp? def print? def new? def create? def permitted_attributes def update_password? def update? def edit? from UserPolicy
+  # def index? def resend_confirmation_instructions? def resend_password_instructions? def export? def confirm_via_otp? def print? def new? def create? def reactivate_account? def permitted_attributes def update_password? def update? def edit? from UserPolicy
 
   def show?
     record.id == user.id
