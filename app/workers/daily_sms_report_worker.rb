af41b58da81638a35227941589a22227f635508d
Milan Patel <milan.patel@sell.do>
2020-12-01 16:09:20 +0530

    Apply suggestions from code review
    - Fixes in daily sms report worker

--- app/workers/daily_sms_report_worker.rb
+++ app/workers/daily_sms_report_worker.rb
@@ -6,7 +6,7 @@ class DailySmsReportWorker
     if current_client.enable_communication['sms'] && current_client.notification_numbers.present?
       record = Client.first
       superadmin = User.where(role: "superadmin").first
-      projects = Project.all
+      projects = record.projects
       projects.each do |project|
         template = Template::SmsTemplate.where(project_id: project.id, name: "daily_sms_report").first
         if template.present?
@@ -16,8 +16,8 @@ class DailySmsReportWorker
             booking_portal_client_id: record.id,
             recipient_id: superadmin.id,
             sms_template_id: template.id,
-            triggered_by_id: record.id,
-            triggered_by_type: record.class.to_s
+            triggered_by_id: project.id,
+            triggered_by_type: project.class.to_s
           )
         end
       end
