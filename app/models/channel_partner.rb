2bc166e8f1d1b05d338e377f58a42d849481f2a4
Milan Patel <milan.patel@sell.do>
2021-01-08 15:55:29 +0530

    Add new expertise options

--- app/models/channel_partner.rb
+++ app/models/channel_partner.rb
@@ -10,7 +10,7 @@ class ChannelPartner
 
   STATUS = %w(active inactive pending rejected)
   THIRD_PARTY_REFERENCE_IDS = %w(reference_id)
-  EXPERTISE = %w( rentals retail )
+  EXPERTISE = %w( rentals retail residential commercial )
   EXPERIENCE = ['0-1 yrs', '1-5 yrs', '5-10 yrs', '10-15 yrs', '15-20 yrs', '20+ yrs']
   DEVELOPERS = [ "Godrej Properties", "Hiranandani", "Lodha Group", "Piramal Realty", "Kanakia", "Mahindra Lifespaces developers", "Kalpataru", "Runwal Group", "Dosti Group", "Wadhwa Group", "Rustomjee", "Puraniks builders", "Adhiraj construction", "L & T Realty", "Arkade Group", "Paradise group", "Chandak", "Marathon Realty", "Raymond Realty", "Damji Shamji Shah", "Shapoorji Pallonji Real Estate", "SD Corp", "Ornate Universal", "Sethia Infrastructure", "Aadi Properties", "Raheja construction", "Tata Housing", "Ajmera", "Adani", "Oberoi Realty", "Acme", "K Raheja", "Hubtown", "Ekta world", "Akshar group", "Raunak group", "JP Infra", "Sunteck Realty Ltd", "Seth Developers", "Kabra developer", "Koltepatil developer" ]
 
