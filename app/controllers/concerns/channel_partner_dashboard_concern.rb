d067703e5003517cefadfd5a9cf9d6797cbd170c
Milan Patel <milan.patel@sell.do>
2021-02-02 21:25:20 +0530

    Fix default date filter on channel partner dashboard. - Changed some data points on cp dashboard.

--- app/controllers/concerns/channel_partner_dashboard_concern.rb
+++ app/controllers/concerns/channel_partner_dashboard_concern.rb
@@ -4,29 +4,45 @@ module ChannelPartnerDashboardConcern
   def channel_partner_dashboard_counts
     #Need refactoring
     dates = params[:dates]
-    dates = (Date.today - 6.months).strftime("%d/%m/%Y") + " - " + Date.today.strftime("%d/%m/%Y") if dates.blank?
-    start_date, end_date = dates.split(' - ')
     project_ids = params["project_ids"].try(:split, ",").try(:flatten) || []
     project_ids = Project.where(id: {"$in": project_ids}).distinct(:id) || []
-    filters = {fltrs: {created_at: dates, project_ids: project_ids}}
-    options = {
-      matcher: {
-        created_at: {
-          "$gte": Date.parse(start_date).beginning_of_day,
-          "$lte": Date.parse(end_date).end_of_day
+
+    filters = if dates.present? || project_ids.present?
+                {fltrs: {created_at: dates, project_ids: project_ids}}
+              else
+                {}
+              end
+    if dates.present? || project_ids.present?
+      start_date, end_date = dates.split(' - ')
+      options = {
+        matcher: {
+          created_at: {
+            "$gte": Date.parse(start_date).beginning_of_day,
+            "$lte": Date.parse(end_date).end_of_day
+          }
         }
       }
-    }
-    options[:matcher][:project_id] = {"$in": project_ids} if project_ids.present?
+      options[:matcher][:project_id] = {"$in": project_ids} if project_ids.present?
+    else
+      options = {}
+    end
     @receipts = current_user.receipts.build_criteria(filters).paginate(page: params[:page] || 1, per_page: params[:per_page])
     @lead_details_labels = get_lead_detail_labels
     @booking_detail_labels = get_booking_detail_labels
     @total_buyers = DashboardDataProvider.total_buyers(current_user, filters)
-    @grouped_receipts = DashboardDataProvider.receipts_group_by(current_user, options)
-    @incentive_approved = (Invoice.where(manager_id: current_user.id, status: 'approved').build_criteria(filters).count)
-    @incentive_generated = (Invoice.where(manager_id: current_user.id, status: {'$ne': 'rejected'}).build_criteria(filters).count)
-    @bookings_eligible_for_brokerage = BookingDetail.nin(id: Invoice.where(manager_id: current_user.id).distinct(:booking_detail_id)).where(status: 'booked_confirmed').filter_by_tasks_completed("registration_done").count
-    @bookins_not_eligible_for_brokerage = BookingDetail.nin(id: Invoice.where(manager_id: current_user.id).distinct(:booking_detail_id)).where(status: {"$ne": 'booked_confirmed'}).filter_by_tasks_pending("registration_done").count
+    #@grouped_receipts = DashboardDataProvider.receipts_group_by(current_user, options)
+    #
+    # Payments section
+    @booking_count = BookingDetail.where(BookingDetail.user_based_scope(current_user)).build_criteria(filters).count
+    @registration_done_booking_count = BookingDetail.where(BookingDetail.user_based_scope(current_user)).build_criteria(filters).filter_by_tasks_completed('registration_done').count
+    @cancelled_booking_count = BookingDetail.where(BookingDetail.user_based_scope(current_user)).build_criteria(filters).in(status: %w(cancelled)).count
+    @confirmed_booking_count = BookingDetail.build_criteria(filters).where(BookingDetail.user_based_scope(current_user)).booked_confirmed.count
+    #
+    # Incentives section
+    @incentive_approved = Invoice.where(manager_id: current_user.id, status: 'approved').build_criteria(filters).count
+    @incentive_raised = Invoice.where(manager_id: current_user.id, status: 'raised').build_criteria(filters).count
+    @bookings_eligible_for_brokerage = BookingDetail.build_criteria(filters).where(BookingDetail.user_based_scope(current_user)).incentive_eligible.count
+    @bookins_not_eligible_for_brokerage = BookingDetail.build_criteria(filters).where(BookingDetail.user_based_scope(current_user)).where(status: {"$in": %w(scheme_approved blocked booked_tentative booked_confirmed)}).filter_by_tasks_pending_tracked_by('system').count
     # @incentive_pending_bookings = DashboardDataProvider.incentive_pending_bookings(current_user, filters)
     # @bookings_with_incentive_generated = Invoice.where(Invoice.user_based_scope(current_user)).build_criteria(filters).distinct(:booking_detail_id).count
     @booking_count_booking_stages = BookingDetail.where(BookingDetail.user_based_scope(current_user)).build_criteria(filters).booking_stages.count
@@ -38,13 +54,16 @@ module ChannelPartnerDashboardConcern
 
   def project_wise_summary
     dates = params[:dates]
-    dates = (Date.today - 6.months).strftime("%d/%m/%Y") + " - " + Date.today.strftime("%d/%m/%Y") if dates.blank?
-    start_date, end_date = dates.split(" - ")
-    options = {
-      matcher: {
-        created_at: {"$gte": Date.parse(start_date).beginning_of_day, "$lte": Date.parse(end_date).end_of_day }
+    if dates.present?
+      start_date, end_date = dates.split(" - ")
+      options = {
+        matcher: {
+          created_at: {"$gte": Date.parse(start_date).beginning_of_day, "$lte": Date.parse(end_date).end_of_day }
+        }
       }
-    }
+    else
+      options = {}
+    end
     @projects_leads = DashboardDataProvider.project_wise_leads_count(current_user, options)
     @projects_av = DashboardDataProvider.project_wise_total_av(current_user, options)
   end
