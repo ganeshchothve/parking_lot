19670cf55ddbe414aae9c6d465b2451f4e52379f
dnyanesh-amuratech <dnyaneshwar.burgute@sell.do>
2021-02-19 20:30:12 +0530

    fixed some minor bugs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -27,22 +27,25 @@ class HomeController < ApplicationController
     unless request.xhr?
       redirect_to (user_signed_in? ? after_sign_in_path : root_path)
     else
-      if @lead.present?
-        CpLeadActivityRegister.create_cp_lead_object(false, @lead, current_user) if current_user.role?("channel_partner")
-      else
-        @project = Project.new(booking_portal_client_id: current_client.id, name: params["project_name"], selldo_id: params["project_id"]) unless @project.present?
-        @user = User.new(booking_portal_client_id: current_client.id, email: params['email'], phone: params['phone'], first_name: params['first_name'], last_name: params['last_name'], lead_id: params[:lead_id], mixpanel_id: params[:mixpanel_id]) unless @user.present?
-        @lead = Lead.new(email: params['email'], phone: params['phone'], first_name: params['first_name'], last_name: params['last_name'], lead_id: params[:lead_id])
-        if @project.save && @user.save
-          @lead.assign_attributes(user_id: @user.id, project_id: @project.id)
-          CpLeadActivityRegister.create_cp_lead_object(true, @lead, current_user) if current_user.role?("channel_partner")
-          if @lead.save
-            format.json { render json: {lead: @lead, success: "Lead created successfully"}, status: :created }
-          else
-            format.json { render json: {errors: @lead.errors.full_messages.uniq}, status: :unprocessable_entity }
-          end
+      respond_to do |format|
+        if @lead.present?
+          CpLeadActivityRegister.create_cp_lead_object(false, @lead, current_user, params[:lead_details]) if current_user.role?("channel_partner")
+          format.json { render json: {lead: @lead, success: "Lead created successfully"}, status: :created }
         else
-          format.json { render json: {errors: (@project.errors.full_messages.uniq.map{|e| "Project - "+ e } rescue []) + (@user.errors.full_messages.uniq.map{|e| "User - "+ e } rescue [])}, status: :unprocessable_entity }
+          @project = Project.new(booking_portal_client_id: current_client.id, name: params["project_name"], selldo_id: params["project_id"]) unless @project.present?
+          @user = User.new(booking_portal_client_id: current_client.id, email: params['email'], phone: params['phone'], first_name: params['first_name'], last_name: params['last_name'], lead_id: params[:lead_id], mixpanel_id: params[:mixpanel_id]) unless @user.present?
+          @lead = Lead.new(email: params['email'], phone: params['phone'], first_name: params['first_name'], last_name: params['last_name'], lead_id: params[:lead_id])
+          if @project.save && @user.save
+            @lead.assign_attributes(user_id: @user.id, project_id: @project.id)
+            CpLeadActivityRegister.create_cp_lead_object(true, @lead, current_user, params[:lead_details]) if current_user.role?("channel_partner")
+            if @lead.save
+              format.json { render json: {lead: @lead, success: "Lead created successfully"}, status: :created }
+            else
+              format.json { render json: {errors: @lead.errors.full_messages.uniq}, status: :unprocessable_entity }
+            end
+          else
+            format.json { render json: {errors: (@project.errors.full_messages.uniq.map{|e| "Project - "+ e } rescue []) + (@user.errors.full_messages.uniq.map{|e| "User - "+ e } rescue [])}, status: :unprocessable_entity }
+          end
         end
       end
     end
@@ -68,10 +71,9 @@ class HomeController < ApplicationController
   end
 
   def set_lead
-    _query = get_query
+    leads = Lead.or(get_query)
     if @project.present?
-      query << {project_id: project.id}
-      @lead = Lead.or(_query).first if _query.present?
+      @lead = leads.where({project_id: @project.id}).first
     end
   end
 
--- app/services/cp_lead_activity_register.rb
+++ app/services/cp_lead_activity_register.rb
@@ -1,10 +1,10 @@
 module CpLeadActivityRegister
-  def self.create_cp_lead_object(new_lead, lead, channel_partner)
-    get_lead_data(lead)
-    if new_lead
+  def self.create_cp_lead_object(new_lead, lead, channel_partner, lead_details = {})
+    get_lead_data(lead, lead_details)
+    if new_lead && lead_details[:lead_already_exists] == 'false'
       new_cp_lead_activity = CpLeadActivity.new(registered_at: Date.today, count_status: "fresh_lead", lead_status: lead.lead_status, expiry_date: Date.today + 45, lead_id: lead.id, user_id: channel_partner.id)
     else
-      if cp_lead_activity = CpLeadActivity.where(lead_id: lead.id, channel_partner_id: channel_partner.id, expiry_date: {"$gt": Date.today}).first
+      if cp_lead_activity = CpLeadActivity.where(lead_id: lead.id, user_id: channel_partner.id, expiry_date: {"$gt": Date.today}).first
         new_cp_lead_activity = CpLeadActivity.new(registered_at: Date.today, count_status: "active_in_same_cp", lead_status: lead.lead_status, expiry_date: Date.today - 1, lead_id: lead.id, user_id: channel_partner.id)
       elsif cp_lead_activity = CpLeadActivity.where(lead_id: lead.id, expiry_date: {"$gt": Date.today}, channel_partner_id: {"$ne": channel_partner.id}).first
         new_cp_lead_activity = CpLeadActivity.new(registered_at: Date.today, count_status: "no_count", lead_status: lead.lead_status, expiry_date: Date.today - 1, lead_id: lead.id, user_id: channel_partner.id)
@@ -18,7 +18,7 @@ module CpLeadActivityRegister
             new_cp_lead_activity = CpLeadActivity.new(registered_at: Date.today, count_status: "accompanied_credit", lead_status: lead.lead_status, expiry_date: Date.today - 1, lead_id: lead.id, user_id: channel_partner.id)
           end
         end
-      elsif lead.lead_stage == 'active'
+      elsif lead.lead_status == 'already_exists'
         if lead.sitevisit_date.present?
           new_cp_lead_activity = CpLeadActivity.new(registered_at: Date.today, count_status: "no_count", lead_status: lead.lead_status, expiry_date: Date.today - 1, lead_id: lead.id, user_id: channel_partner.id)
         else
@@ -26,14 +26,17 @@ module CpLeadActivityRegister
         end
       end
     end
+    new_cp_lead_activity.try(:save)
   end
 
-  def self.get_lead_data(lead)
-    lead.lead_stage = "registered"
-    lead.lead_status = "active"
-    lead.lead_lost_date = nil
-    lead.sitevisit_status = "conducted"
-    lead.sitevisit_date = Date.today - 4
+  def self.get_lead_data(lead, lead_details = {})
+    lead.lead_status =  lead_details[:lead_already_exists] == 'true' ? "already_exists" : "registered"
+    lead.lead_stage = lead_details[:stage]
+    lead.lead_lost_date = (Time.zone.parse(lead_details[:stage_changed_on]) rescue nil) if lead.lead_stage.in?(%w(lost unqualified))
+    if lead_details[:last_sv_conducted_on].present?
+      lead.sitevisit_status = "conducted"
+      lead.sitevisit_date = (Time.zone.parse(lead_details[:last_sv_conducted_on]) rescue nil)
+    end
     lead.save
   end
 end
