23b01edf80bb61908dc0a110e598615b12b89c4a
Milan Patel <milan.patel@sell.do>
2021-04-05 22:26:39 +0530

    DLT Changes

--- app/policies/admin/template/sms_template_policy.rb
+++ app/policies/admin/template/sms_template_policy.rb
@@ -3,7 +3,7 @@ class Admin::Template::SmsTemplatePolicy < Template::SmsTemplatePolicy
   def permitted_attributes params={}
     attributes = super
     if user.role?('superadmin')
-      attributes += [:temp_id, :dlt_header_id, :dlt_temp_id, :template_variables]
+      attributes += [:dlt_tag_id, :dlt_temp_id, :template_variables]
       attributes += [template_variables_attributes: TemplateVariablePolicy.new(user, TemplateVariable.new).permitted_attributes]
     end
     attributes
