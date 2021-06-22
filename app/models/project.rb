4c95bba7c1fd7804b56e46aa60f498bf1a998076
Ketan Sabnis <ketan.sabnis@gmail.com>
2021-06-22 12:57:56 +0530

    Implemented Project Timeline basics - Pending on Images for timeline

--- app/models/project.rb
+++ app/models/project.rb
@@ -137,13 +137,14 @@ class Project
   has_many :specifications
   has_many :offers
   has_many :incentive_schemes
+  has_many :timeline_updates
 
   validates :name, presence: true
   validates_uniqueness_of :name, :rera_registration_no, allow_blank: true
   validates :enable_actual_inventory, array: { inclusion: {allow_blank: true, in: (User::ADMIN_ROLES + User::BUYER_ROLES) } }
   validates :ga_code, format: {with: /\Aua-\d{4,9}-\d{1,4}\z/i, message: 'is not valid'}, allow_blank: true
 
-  accepts_nested_attributes_for :specifications, :offers, :address, allow_destroy: true
+  accepts_nested_attributes_for :specifications, :offers, :timeline_updates, :address, allow_destroy: true
   index(client_id:1)
 
   default_scope -> { where(is_active: true)}
---
+++ app/models/timeline_update.rb
@@ -0,0 +1,19 @@
+class TimelineUpdate
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  include ArrayBlankRejectable
+  include InsertionStringMethods
+  include ApplicationHelper
+  extend ApplicationHelper
+
+  DOCUMENT_TYPES = %w[images].freeze
+
+  field :name, type: String
+  field :description, type: String
+  field :date, type: Date
+  
+  belongs_to :project
+  has_many :assets, as: :assetable
+
+  validates :name, :description, :date, presence: true
+end
