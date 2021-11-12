c5b651e9055e2d6bb3c70c05baad8217c0f198b2
Milan Patel <milan.patel@sell.do>
2021-11-12 15:00:56 +0530

    Add projects on landing page from backend

--- app/models/project.rb
+++ app/models/project.rb
@@ -155,7 +155,7 @@ class Project
   has_many :videos, as: :videoable
   has_many :nearby_locations
 
-  validates :name, presence: true
+  validates :name, :configurations, presence: true
   validates_uniqueness_of :name, :rera_registration_no, allow_blank: true
   validates :enable_actual_inventory, array: { inclusion: {allow_blank: true, in: (User::ADMIN_ROLES + User::BUYER_ROLES) } }
   validates :ga_code, format: {with: /\Aua-\d{4,9}-\d{1,4}\z/i, message: 'is not valid'}, allow_blank: true
