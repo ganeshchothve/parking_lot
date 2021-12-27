aada5bee8784bb4ef541a91a7daa7128150e4d7d
shubham-alavni-selldo <shubham.alvani@sell.do>
2021-12-27 15:22:58 +0530

    [LAUNCHPAD-124] - CSS changes added and @options variable changed in dashbaord concern

--- app/controllers/concerns/booking_detail_dashboard_concern.rb
+++ app/controllers/concerns/booking_detail_dashboard_concern.rb
@@ -1,29 +1,30 @@
 module BookingDetailDashboardConcern
   def booking_details_counts
-    set_matcher
-    @booking_details_data = DashboardDataProvider.booking_details_data(@options)
+    options = set_matcher
+    @booking_details_data = DashboardDataProvider.booking_details_data(options)
   end
 
   def set_matcher
-    @options = {matcher: {}}
+    options = {matcher: {}}
     if params[:dates].present?
       @dates = params[:dates].split(' - ')
-      @start_date, @end_date = @dates
-      @options[:matcher] = {
+      start_date, end_date = @dates
+      options[:matcher] = {
         booked_on: {
-          "$gte": Date.parse(@start_date).beginning_of_day,
-          "$lte": Date.parse(@end_date).end_of_day
+          "$gte": Date.parse(start_date).beginning_of_day,
+          "$lte": Date.parse(end_date).end_of_day
         }
       }
     end
     if params[:project_ids].present?
-      @options[:matcher][:project_id] = {"$in": params[:project_ids].map{|id| BSON::ObjectId(id) }}
+      options[:matcher][:project_id] = {"$in": params[:project_ids].map{|id| BSON::ObjectId(id) }}
     else
-      @options[:matcher][:project_id] = {"$in": Project.all.pluck(:_id).uniq }
+      options[:matcher][:project_id] = {"$in": Project.all.pluck(:_id).uniq }
     end
     if params[:source].present?
-      @options[:matcher][:source] = params[:source]
+      options[:matcher][:source] = params[:source]
     end
+    options
   end
 
 end
