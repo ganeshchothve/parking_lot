1d2ece5d29d1b23be977118094d2c32c5c0a953b
Milan Patel <milan.patel@sell.do>
2021-12-27 19:23:23 +0530

    Fix event observer worker to handle serialization of bson objectid & add meetint image url in payload if present

--- app/workers/meeting_observer_worker.rb
+++ app/workers/meeting_observer_worker.rb
@@ -10,11 +10,11 @@ class MeetingObserverWorker
         payload = {
           'project' => meeting.project&.as_json,
           'campaign' => meeting.campaign&.as_json,
-          'meeting' => meeting.as_json
+          'meeting' => meeting.as_json(include: :assets)
         }.merge(changes || {})
 
         if changes.present? && changes.has_key?('participant_ids')
-          event_name = changes.dig('participant_ids', 1)&.include?(user.id) ? 'Event Subscribed' : 'Event Unsubscribed'
+          event_name = changes.dig('participant_ids', 1)&.map(&:to_s)&.include?(user.id.to_s) ? 'Event Subscribed' : 'Event Unsubscribed'
           Crm::Api::ExecuteWorker.perform_async('post', 'User', user.id, event_name, payload)
         end
       end
