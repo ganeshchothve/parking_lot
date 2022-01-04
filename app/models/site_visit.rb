12430f9f28749fa8a8540d04ace96664e93a2a1f
dnyanesh-amuratech <dnyaneshwar.burgute@sell.do>
2022-01-04 12:40:22 +0530

    [lp-128] sv status update issue fix

--- app/models/site_visit.rb
+++ app/models/site_visit.rb
@@ -164,7 +164,7 @@ class SiteVisit
   private
 
   def validate_scheduled_on_datetime
-    self.errors.add :base, 'Scheduled On should not be past date' if self.scheduled_on.beginning_of_day < Time.now.beginning_of_day
+    self.errors.add :base, 'Scheduled On should not be past date' if (self.scheduled_on_changed? && self.scheduled_on < Time.current.beginning_of_day)
   end
 
   def existing_scheduled_sv
