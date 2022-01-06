f984db65e34eed90cc4ba39a4e9213973771149a
bonish-selldo <bonish.agarwal@sell.do>
2022-01-06 17:07:24 +0530

    [LAUNCHPAD-148] - Add contact details on project

--- app/policies/admin/project_policy.rb
+++ app/policies/admin/project_policy.rb
@@ -62,7 +62,7 @@ class Admin::ProjectPolicy < ProjectPolicy
         :selldo_channel_partner_form_id, :selldo_api_key, :selldo_default_srd, :selldo_cp_srd,
         :allowed_bookings_per_user, :helpdesk_number, :helpdesk_email, :ga_code, :gtm_tag,
         :notification_email, :notification_numbers, :sender_email, :area_unit,
-        :support_number, :support_email, :channel_partner_support_number, :channel_partner_support_email, :cancellation_amount, :blocking_amount, :region, :sv_incentive, :spot_booking_incentive, :pre_reg_incentive_percentage, :pre_reg_min_bookings, :iris_url,
+        :support_number, :support_email, :channel_partner_support_number, :channel_partner_support_email, :cancellation_amount, :blocking_amount, :region, :sv_incentive, :spot_booking_incentive, :pre_reg_incentive_percentage, :pre_reg_min_bookings, :iris_url, :support_name, :support_mail, :support_contact,
         :blocking_days, :enable_slot_generation, :holding_minutes, :terms_and_conditions, :email_header, :email_footer, :embed_map_tag, :hot, :price_starting_from, :price_upto, :enable_inventory, :enable_booking_with_kyc, :gst_slab_applicable, :check_sv_availability_in_selldo, third_party_references_attributes: ThirdPartyReferencePolicy.new(user, ThirdPartyReference.new).permitted_attributes,
         email_domains: [], booking_portal_domains: [], enable_actual_inventory: [], enable_live_inventory: [], incentive_percentage_slabs: [], incentive_gst_slabs: [], booking_sources: [], incentive_calculation: []
       ]
