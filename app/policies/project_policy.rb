a2e0e03bed3c4444ec6d4126537d9574af565b18
SubhashGupta-SellDo <subhash.gupta@sell.do>
2022-02-01 12:47:22 +0530

    Changes as suggested in PR

--- app/policies/project_policy.rb
+++ app/policies/project_policy.rb
@@ -1,5 +1,2 @@
-class ProjectPolicy < ApplicationPolicy
-  def switch_project?
-    ( !user.role.in?(User::ALL_PROJECT_ACCESS + %w(channel_partner)) || user.buyer? ) && user.project_ids.count > 1
-  end
+class ProjectPolicy < ApplicationPolicy 
 end
