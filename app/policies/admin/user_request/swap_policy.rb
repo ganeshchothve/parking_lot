9fce77a1b25ec492fdbfb3684fe41b1f41c1cbfe
SubhashGupta-SellDo <subhash.gupta@sell.do>
2022-02-03 14:08:25 +0530

    Access for cancellation and swap to Sales Head.

--- app/policies/admin/user_request/swap_policy.rb
+++ app/policies/admin/user_request/swap_policy.rb
@@ -15,8 +15,8 @@ class Admin::UserRequest::SwapPolicy < Admin::UserRequestPolicy
 
   def permitted_attributes(params = {})
     attributes = super
-    attributes += [:requestable_id, :requestable_type] if record.new_record? && record.status == 'pending' && %w[admin crm sales superadmin cp].include?(user.role)
-    attributes += [:alternate_project_unit_id] if %w[admin crm sales superadmin cp channel_partner].include?(user.role) && record.status == 'pending'
+    attributes += [:requestable_id, :requestable_type] if record.new_record? && record.status == 'pending' && %w[admin crm sales superadmin cp sales_admin].include?(user.role)
+    attributes += [:alternate_project_unit_id] if %w[admin crm sales superadmin cp channel_partner sales_admin].include?(user.role) && record.status == 'pending'
     attributes
   end
 end
