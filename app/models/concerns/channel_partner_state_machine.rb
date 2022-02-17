5f1dacef0564bbb6f1da929ee7b9518b66dc4834
SubhashGupta-SellDo <subhash.gupta@sell.do>
2022-02-17 14:50:13 +0530

    Changes

--- app/models/concerns/channel_partner_state_machine.rb
+++ app/models/concerns/channel_partner_state_machine.rb
@@ -70,7 +70,6 @@ module ChannelPartnerStateMachine
           )
         end
       end
-
       # send notification
       user = self.users.first
       if user.present? && user.role?('channel_partner')
@@ -84,7 +83,7 @@ module ChannelPartnerStateMachine
 
     def send_push_notification template_name, recipient
       template = Template::NotificationTemplate.where(name: template_name).first
-      if template.present? && user.booking_portal_client.notification_enabled?
+      if template.present? && users.first&.booking_portal_client.notification_enabled?
         push_notification = PushNotification.new(
           notification_template_id: template.id,
           triggered_by_id: self.id,
