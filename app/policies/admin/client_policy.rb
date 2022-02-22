fd92ffdf66568a64e41e31ac739a74042ceea663
dnyanesh-amuratech <dnyaneshwar.burgute@sell.do>
2022-02-22 15:25:29 +0530

    [LAUNCHPAD-232] up twilio for launchpad

--- app/policies/admin/client_policy.rb
+++ app/policies/admin/client_policy.rb
@@ -20,6 +20,7 @@ class Admin::ClientPolicy < ClientPolicy
   def permitted_attributes(params = {})
     attributes = super
     if %w[superadmin].include?(user.role)
+      attributes += [:twilio_account_sid, :twilio_auth_token]
       attributes += [general_user_request_categories: [], partner_regions: [], roles_taking_registrations: [], mask_lead_data_for_roles: []]
     end
     attributes.uniq
