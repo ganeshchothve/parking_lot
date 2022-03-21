6ec930048871f48eec92a2ed3222e08e45db5d9a
shubham-alavni-selldo <shubham.alvani@sell.do>
2022-03-21 18:44:20 +0530

    [LAUNCHPAD-284] - build_criteria changes added

--- app/models/concerns/filter_by_criteria.rb
+++ app/models/concerns/filter_by_criteria.rb
@@ -4,8 +4,12 @@ module FilterByCriteria
   def build_criteria params={}
     params ||= {}
     filters = self.all
-    parameter_filters = ActionController::Parameters.new(params[:fltrs] || {})
-    (parameter_filters.permit!&.to_h || {}).each do |key, value|
+    if params.is_a?(Hash)
+      filter_parameters = ActionController::Parameters.new(params[:fltrs] || {}).try(:permit!)
+    else
+      filter_parameters = params[:fltrs].try(:permit!)
+    end
+    (filter_parameters&.to_h || {}).each do |key, value|
       if value.present? && self.respond_to?("filter_by_#{key}")
         filters = filters.send("filter_by_#{key}", (value == 'nil' ? nil : value))
       end
