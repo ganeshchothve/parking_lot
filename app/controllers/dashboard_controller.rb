d1e68925b276f057464963d5682583f0fbda575c
shubham-alavni-selldo <shubham.alvani@sell.do>
2022-04-04 16:47:59 +0530

    action authenticated

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,7 +5,7 @@ class DashboardController < ApplicationController
   include BookingDetailDashboardConcern
   include RevenueReportDashboardConcern
   include ChannelPartnerLeaderboardConcern
-  before_action :authenticate_user!, only: [:index, :documents]
+  before_action :authenticate_user!, only: [:index, :documents, :dashboard_landing_page, :channel_partners_leaderboard, :channel_partners_leaderboard_without_layout]
   before_action :set_lead, only: :index, if: proc { current_user.buyer? }
 
   layout :set_layout
