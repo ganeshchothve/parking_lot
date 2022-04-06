fb99570de837d869b434605a96e8cd666dce6517
Milan Patel <milan.patel@sell.do>
2022-04-06 14:41:16 +0530

    Add helpers for mobile policy setup - Send policies in sitevisit list api - Move project policies for mobile into helpers

---
+++ app/helpers/site_visits_helper.rb
@@ -0,0 +1,17 @@
+module SiteVisitsHelper
+  def allow_reschedule?(site_visit)
+    policy([:admin, site_visit]).edit? && policy([:admin, site_visit]).editable_field?('scheduled_on')
+  end
+
+  def allow_add_notes?(site_visit)
+    policy([:admin, Note.new(notable: site_visit)]).new?
+  end
+
+  def allow_state_change?(site_visit)
+    policy([current_user_role_group, site_visit]).change_state? && policy([current_user_role_group, site_visit]).editable_field?('event')
+  end
+
+  def allow_approval_state_change?(site_visit)
+    policy([current_user_role_group, site_visit]).change_state? && policy([current_user_role_group, site_visit]).editable_field?('approval_event')
+  end
+end
--- app/policies/admin/booking_detail_policy.rb
+++ app/policies/admin/booking_detail_policy.rb
@@ -53,7 +53,7 @@ class Admin::BookingDetailPolicy < BookingDetailPolicy
 
   def show_add_booking_link?
     out = !enable_inventory? && record.try(:user).try(:buyer?) && %w[account_manager channel_partner cp_owner].include?(user.role) && record.lead&.project&.bookings_enabled?
-    out = false if user.role.in?(%w(cp_owner channel_partner)) && !interested_project_present?
+    out = false if user.role.in?(%w(cp_owner channel_partner)) && !(user.active_channel_partner? && interested_project_present?)
     out
   end
 
