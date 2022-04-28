657cfdc3d8543c4bd0db38ac74960d96aa5da0f6
shubham-alavni-selldo <shubham.alvani@sell.do>
2022-04-28 13:54:24 +0530

    [LAUNCHPAD-361] - project id removed from notification template

--- app/models/concerns/invoice_state_machine.rb
+++ app/models/concerns/invoice_state_machine.rb
@@ -115,7 +115,7 @@ module InvoiceStateMachine
     end
 
     def send_push_notification template_name, recipient
-      template = Template::NotificationTemplate.where(project_id: self.project.id, name: template_name).first
+      template = Template::NotificationTemplate.where(name: template_name).first
       if template.present? && template.is_active? && recipient.booking_portal_client.notification_enabled?
         push_notification = PushNotification.new(
           notification_template_id: template.id,
--- app/models/concerns/site_visit_state_machine.rb
+++ app/models/concerns/site_visit_state_machine.rb
@@ -78,7 +78,7 @@ module SiteVisitStateMachine
     end
 
     def send_push_notification template_name, recipient
-      template = Template::NotificationTemplate.where(project_id: self.lead.project_id,name: template_name).first
+      template = Template::NotificationTemplate.where(name: template_name).first
       if template.present? && template.is_active? && recipient.booking_portal_client.notification_enabled?
         push_notification = PushNotification.new(
           notification_template_id: template.id,
