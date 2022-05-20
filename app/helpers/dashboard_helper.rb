817dac66678a274d8af0d6d04e4f5848537f0f93
ganeshchothve-selldo <ganech.chothve@sell.do>
2022-05-20 10:01:36 +0530

    Defined 2 methods in dashboard helper

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -4,16 +4,25 @@ module DashboardHelper
   end
 
   #
+  # Extract percentage from formula to readable string
+  # 0.02 * self.calculate_agreement_price => 2%
+  #
+
+  def fetch_percentage(formula)
+    percentage = formula.scan(/\d+[,.]\d+/)[0]
+    number_to_percentage((percentage = percentage.to_f * 100), precision: (percentage == percentage.to_i ? 0 : 2))
+  end
+
+  #
   # Convert formula to readable string
   # 0.02 * self.calculate_agreement_price => 2% of Agreement value
   #
+
   def formula_to_human(formula)
     percentage = formula.scan(/\d+[,.]\d+/)[0]
-
-    #value_of = formula.scan(/agreement_price|all_inclusive_price/)
-    #result = [percentage, value_of].flatten.map
-      #.with_index { |x, i| i.zero? ? "<strong>#{number_to_percentage((_x = x.to_f * 100), precision: (_x == _x.to_i ? 0 : 2))}</strong>" : t("formula.#{x}") }.join(' of ').html_safe
-    #result.presence || formula
-    number_to_percentage((percentage = percentage.to_f * 100), precision: (percentage == percentage.to_i ? 0 : 2))
+    value_of = formula.scan(/agreement_price|all_inclusive_price/)
+    result = [percentage, value_of].flatten.map
+      .with_index { |x, i| i.zero? ? "<strong>#{number_to_percentage((_x = x.to_f * 100), precision: (_x == _x.to_i ? 0 : 2))}</strong>" : t("formula.#{x}") }.join(' of ').html_safe
+    result.presence || formula
   end
 end
