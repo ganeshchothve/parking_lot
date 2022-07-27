5d44d0988037319d81ee4d630d6c85e3e4c98e3b
bonish-selldo <bonish.agarwal@sell.do>
2022-07-27 12:05:53 +0530

    [mp-bug-fixes] - Extra bullet point error fixed

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -60,7 +60,7 @@ class Admin::ProjectsController < AdminController
         format.json { render json: @project, status: :created }
       else
         errors = @project.errors.full_messages
-        errors << @project.specifications.collect{|x| x.errors.full_messages}
+        errors << @project.specifications.collect{|x| x.errors.full_messages}  if @project.specifications.present?
         errors.uniq!
         format.html { render :new }
         format.json { render json: { errors: errors }, status: :unprocessable_entity }
