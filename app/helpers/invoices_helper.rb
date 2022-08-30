96020eb693c9bdf07887df11ade1571131e5baa8
bonish-selldo <bonish.agarwal@sell.do>
2022-08-30 16:30:13 +0530

    [LAUNCHPAD-633] - handled incentive scheme categories on invoice filters on basis of lead flag

--- app/helpers/invoices_helper.rb
+++ app/helpers/invoices_helper.rb
@@ -2,4 +2,17 @@ module InvoicesHelper
   def available_events record
     record.aasm.events(permitted: true).collect{|x| x.name.to_s}
   end
+
+  def filter_incentive_categories
+    categories = IncentiveScheme::CATEGORIES
+    resultant_categories = []
+    if !current_client.enable_leads?
+      resultant_categories = categories.reject{|x| x == 'lead'}
+    end
+    if resultant_categories.present?
+      resultant_categories.collect {|x| [t("mongoid.attributes.invoice/categories.#{x}"), x]}
+    else
+      categories.collect {|x| [t("mongoid.attributes.invoice/categories.#{x}"), x]}
+    end
+  end
 end
