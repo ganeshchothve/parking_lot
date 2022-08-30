8b1a8daebfa6ce3985868b14c53ed0a31e24af8e
shubham-alavni-selldo <shubham.alvani@sell.do>
2022-08-30 20:09:32 +0530

    create custom field service added

---
+++ app/services/kylas/update_custom_field.rb
@@ -0,0 +1,78 @@
+module Kylas
+  class UpdateCustomField
+
+    attr_reader :user, :cp_user, :options
+
+    def initialize(user, cp_user, options = {})
+      @user = user
+      @cp_user = cp_user
+      @options = options
+    end
+
+    def call
+      return unless user.present? && cp_user.present? && options.present?
+      begin
+        url = URI("#{APP_KYLAS_HOST}/#{APP_KYLAS_VERSION}/entities/#{options[:entity]}/fields/#{options[:field_id]}?fieldId=#{options[:field_id]}")
+        https = Net::HTTP.new(url.host, url.port)
+        https.use_ssl = true
+        request = Net::HTTP::Put.new(url)
+        if user.kylas_api_key?
+          request['api-key'] = user.kylas_api_key
+        else
+          request['Authorization'] = "Bearer #{user.fetch_access_token}"
+        end
+        request['Content-Type'] = 'application/json'
+        request['Accept'] = 'application/json'
+        request.body = JSON.dump(custom_field_params)
+        response = https.request(request)
+
+        case response
+        when Net::HTTPOK, Net::HTTPSuccess
+          ApiLog.log_responses(url, [custom_field_params], [JSON.parse(response.body)], cp_user, 'Hash', user.booking_portal_client)
+          { success: true, data: JSON.parse(response.body) }
+        when Net::HTTPBadRequest
+          ApiLog.log_responses(url, [custom_field_params], [(response.message rescue "Invalid Data!")], cp_user, 'Hash', user.booking_portal_client)
+          { success: false, message: "Invalid data!" }
+        when Net::HTTPNotFound
+          ApiLog.log_responses(url, [custom_field_params], [(response.message rescue "Invalid Data!")], cp_user, 'Hash', user.booking_portal_client)
+          { success: false, message: "Invalid data!" }
+        when Net::HTTPServerError
+          ApiLog.log_responses(url, [custom_field_params], [(response.message rescue "Invalid Data!")], cp_user, 'Hash', user.booking_portal_client)
+          { success: false, message: "Invalid data!" }
+        when Net::HTTPUnauthorized
+          ApiLog.log_responses(url, [custom_field_params], [(response.message rescue "Unauthorized!")], cp_user, 'Hash', user.booking_portal_client)
+          { success: false, message: "Unauthorized!" }
+        else
+          ApiLog.log_responses(url, [custom_field_params], [(response.message rescue "Invalid data!")], cp_user, 'Hash', user.booking_portal_client)
+          { success: false, message: "Invalid data!" }
+        end
+      rescue => e
+        ApiLog.log_responses(url, [custom_field_params], [e.message], cp_user, 'Hash', user.booking_portal_client)
+        { success: false, error: e.message }
+      end
+    end
+
+    private
+    def custom_field_params
+      {
+          displayName: 'Channel Partner',
+          description: 'List of Channel Partners',
+          pickLists: [
+            {
+              id: nil, 
+              name: nil, 
+              displayName: cp_user.name,
+            }
+          ],
+          filterable: false,
+          sortable: false,
+          standard: false,
+          required: false,
+          type: 'PICK_LIST',
+          important: false 
+      }
+    end
+
+  end
+  
+end
