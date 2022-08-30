f00c076ca124af0ab06b5337b9a0f43cbde7c1d2
bonish-selldo <bonish.agarwal@sell.do>
2022-08-30 15:13:52 +0530

    [LAUNCHPAD-633] - Bulk Upload Report dependency on CP and Lead flags

--- app/helpers/bulk_upload_reports_helper.rb
+++ app/helpers/bulk_upload_reports_helper.rb
@@ -5,13 +5,26 @@ module BulkUploadReportsHelper
 
   def filter_bulk_upload_report
     bulk_upload_docs = BulkUploadReport::DOCUMENT_TYPES
-    if !current_client.enable_channel_partners?
-      bulk_upload_docs.reject!{|doc| %w(channel_partners channel_partner_manager_change).include?(doc)}
+    resultant_docs = []
+
+    if !current_client.enable_channel_partners? && current_client.enable_leads?
+      resultant_docs = reject_cp_docs(bulk_upload_docs)
+    elsif current_client.enable_channel_partners? && !current_client.enable_leads?
+      resultant_docs = reject_lead_docs(bulk_upload_docs)
+    elsif !current_client.enable_channel_partners? && !current_client.enable_leads?
+      resultant_docs = reject_cp_docs(bulk_upload_docs) & reject_lead_docs(bulk_upload_docs)
+    else
+      resultant_docs = bulk_upload_docs
     end
-    if !current_client.enable_leads?
-      bulk_upload_docs.reject!{|doc| %w(leads receipts).include?(doc)}
-    end
-    options = bulk_upload_docs.collect{ |doc| [t("mongoid.attributes.bulk_upload_report/file_types.#{doc}"), doc] }
+    options = resultant_docs.collect{ |doc| [t("mongoid.attributes.bulk_upload_report/file_types.#{doc}"), doc] }
     options
+  end
+
+  def reject_cp_docs bulk_upload_docs
+    resultant_cp_docs = bulk_upload_docs.reject{|doc| %w(channel_partners channel_partner_manager_change).include?(doc)}
+  end
+
+  def reject_lead_docs bulk_upload_docs
+    resultant_lead_docs = bulk_upload_docs.reject{|doc| %w(leads receipts).include?(doc)}
   end
 end
