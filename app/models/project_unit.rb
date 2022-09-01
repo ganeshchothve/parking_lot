1e9ee55dca901955750da5b6f7f884d7681275b4
ganeshchothve-selldo <ganech.chothve@sell.do>
2022-09-01 14:43:43 +0530

    [LAUNCHPAD-638]-Local Configuration

--- app/models/project_unit.rb
+++ app/models/project_unit.rb
@@ -84,8 +84,8 @@ class ProjectUnit
 
   validates :saleable, :carpet, :base_rate, :agreement_price, :all_inclusive_price, :project_id, :project_tower_id, :unit_configuration_id, :floor, :floor_order, :bathrooms, :type, :project_name, :project_tower_name, :unit_configuration_name, :bedrooms, presence: true
   validates :status, :name, :erp_id, presence: true
-  validates :status, inclusion: { in: proc { ProjectUnit.available_statuses.collect { |x| x[:id] } } }
-  validates :available_for, inclusion: { in: proc { ProjectUnit.available_available_fors.collect { |x| x[:id] } } }
+  validates :status, inclusion: { in: proc { I18n.t("mongoid.attributes.project_unit/status").collect{|k,v| k.to_s} } }
+  validates :available_for, inclusion: { in: proc { I18n.t("mongoid.attributes.project_unit/available_for").collect{|k,v| k.to_s} } }
   validates :saleable, :carpet, :base_rate, :numericality => {:greater_than => 0}, if: :valid_status?
   validates :floor_order, uniqueness: { scope: [:project_tower_id, :floor] }
   validates :erp_id, uniqueness: { scope: [:project_id] }
@@ -228,31 +228,6 @@ class ProjectUnit
         return 'not_available'
       end
     end
-  end
-
-  def self.available_statuses
-    out = [
-      { id: 'available', text: 'Available' },
-      { id: 'not_available', text: 'Not Available' },
-      { id: 'error', text: 'Error' },
-      { id: 'hold', text: 'Hold' },
-      { id: 'blocked', text: 'Blocked' }
-    ]
-    if current_client.enable_company_users?
-      out += [
-        { id: 'management', text: 'Management Blocking' },
-        { id: 'employee', text: 'Employee Blocking' }
-      ]
-    end
-    out
-  end
-
-  def self.available_available_fors
-    [
-      { id: 'user', text: 'User' },
-      { id: 'management', text: 'Management' },
-      { id: 'employee', text: 'Employee' }
-    ]
   end
 
   def self.booking_stages
