49fe413e9ece769df14c4ab8ec889dc6f5162b42
bonish-selldo <bonish.agarwal@sell.do>
2022-09-10 10:01:11 +0530

    [LAUNCHPAD-663] - Removed Channel Partner Manager Phone header from channel partner user import - Tagging the manager on user from partner company

--- app/services/bulk_upload/channel_partner_user.rb
+++ app/services/bulk_upload/channel_partner_user.rb
@@ -2,7 +2,7 @@ module BulkUpload
   class ChannelPartnerUser < Base
     def initialize(bulk_upload_report)
       super(bulk_upload_report)
-      @correct_headers = ['Company Id', 'First Name', 'Last Name', 'Phone', 'Email', 'Rera', 'Channel Partner Manager Phone']
+      @correct_headers = ['Company Id', 'First Name', 'Last Name', 'Phone', 'Email', 'Rera']
     end
 
     def process_csv(csv)
@@ -21,29 +21,19 @@ module BulkUpload
             attrs[:email] = email if email.present?
             attrs[:phone] = phone if phone.present?
             attrs[:rera_id] = row.field(5).to_s.strip
-            if m_phone = row.field(6).to_s.strip.presence
-              _manager_phone = Phonelib.parse(m_phone)
-              manager_phone = (_manager_phone.country_code == '91' && _manager_phone.sanitized.length == 10 ? "+91#{_manager_phone.sanitized}" : "+#{_manager_phone.sanitized}")
-              manager = User.where(phone: manager_phone, role: {'$in': ['cp']}).first if manager_phone.present?
-              if manager
-                attrs[:manager_id] = manager.id
-                attrs[:channel_partner_id] = partner_company.id
-                attrs[:role] = 'channel_partner'
-                user = User.new(attrs)
-                if user.save
-                  bur.success_count += 1
-                else
-                  (bur.upload_errors.find_or_initialize_by(row: row.fields).messages.push(*user.errors.full_messages)).uniq
-                  bur.failure_count += 1
-                end
-              else
-                (bur.upload_errors.find_or_initialize_by(row: row.fields).messages.push("Channel partner Manager not found with Phone no: #{m_phone}")).uniq
-                bur.failure_count += 1
-              end
+            attrs[:manager_id] = partner_company.manager_id if partner_company.manager_id.present?
+            attrs[:channel_partner_id] = partner_company.id
+            attrs[:role] = 'channel_partner'
+            user = User.new(attrs)
+            if user.save
+              bur.success_count += 1
             else
-              (bur.upload_errors.find_or_initialize_by(row: row.fields).messages.push("Channel partner Manager Phone Number not found")).uniq
+              (bur.upload_errors.find_or_initialize_by(row: row.fields).messages.push(*user.errors.full_messages)).uniq
               bur.failure_count += 1
             end
+          else
+            (bur.upload_errors.find_or_initialize_by(row: row.fields).messages.push("Partner Company Not Found")
+            bur.failure_count += 1
           end
         end
       end
