563ef514060ff5d92ef06b6f50a2a08f186d65d7
shubham-alavni-selldo <shubham.alvani@sell.do>
2022-09-27 18:28:43 +0530

    [IRISMP-67] - Admin auto confirm

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -26,7 +26,13 @@ class Admin::UsersController < AdminController
       @user = User.new(role: 'admin')
       @user.assign_attributes(user_params)
       @user.assign_attributes(booking_portal_client: @client)
+      @user.temporary_password = generate_password * 2
+      @user.assign_attributes(password: @user.temporary_password, password_confirmation: @user.password_confirmation)
+      @user.skip_confirmation_notification!
       if @user.save
+        binding.pry
+        @user.confirm
+        @user.send_generated_password_email
         format.html { redirect_to new_user_session_path, notice: 'Successfully registered' }
       else
         format.html { redirect_to user_signup_path, alert: @user.errors.full_messages }
