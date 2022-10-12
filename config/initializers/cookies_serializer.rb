5fb6298f27326e744a6b9a04ba7d4ea7e05db972
shubham-alavni-selldo <shubham.alvani@sell.do>
2022-10-12 20:51:14 +0530

    changes added in tld_length

--- config/initializers/cookies_serializer.rb
+++ config/initializers/cookies_serializer.rb
@@ -3,4 +3,4 @@
 # Specify a serializer for the signed and encrypted cookie jars.
 # Valid options are :json, :marshal, and :hybrid.
 Rails.application.config.action_dispatch.cookies_serializer = :json
-Rails.application.config.session_store :cookie_store, key: '_app_session', expire_after: nil, domain: :all, tld_length: (ENV_CONFIG[:session_tld_length] || 1)
+Rails.application.config.session_store :cookie_store, key: '_app_session', expire_after: nil, domain: :all, tld_length: (ENV_CONFIG[:session_tld_length] || 2)
