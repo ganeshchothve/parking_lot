280566a5ef8e2f95963b53e2732f530b626d3f81
shubham-alavni-selldo <shubham.alvani@sell.do>
2022-10-19 16:33:11 +0530

    select_client changes added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -40,7 +40,7 @@ class HomeController < ApplicationController
 
   def select_client
     if current_user.role.in?(%w(superadmin))
-      if current_client.present?
+      if current_client.present? && (Client.in(booking_portal_domains: current_domain).present? || current_client.projects.in(booking_portal_domains: current_domain).present?)
         current_user.update(selected_client_id: current_client.id)
         redirect_to home_path(current_user)
       elsif current_user.client_ids.count == 1
