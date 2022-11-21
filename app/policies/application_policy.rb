2d5baf8206efc2c51fa64cbe286532ffd2d8c23e
Milan Patel <milan.patel@sell.do>
2022-11-21 19:36:22 +0530

    Fix CP registration

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -4,15 +4,17 @@ class ApplicationPolicy
   attr_reader :user, :record, :condition, :current_client, :current_project
 
   def initialize(user_context, record)
-    if user_context.is_a?(User)
-      user = user_context
-      @user = user
-      @current_client = user.role?('superadmin') ? user.selected_client : user.booking_portal_client
-      @current_project = nil
-    else
-      @user = user_context.user
-      @current_client = user_context.current_client
-      @current_project = user_context.current_project
+    if user_context.present?
+      if user_context.is_a?(User)
+        user = user_context
+        @user = user
+        @current_client = user.role?('superadmin') ? user.selected_client : user.booking_portal_client
+        @current_project = nil
+      else
+        @user = user_context.user
+        @current_client = user_context.current_client
+        @current_project = user_context.current_project
+      end
     end
     @record = record
   end
