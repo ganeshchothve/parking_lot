78ecf7fdd71ce63e94e304e292432065d264602a
bonish-selldo <bonish.agarwal@sell.do>
2022-11-24 20:11:33 +0530

    Show crm reference id on lead only in marketplace apps

--- app/models/project.rb
+++ app/models/project.rb
@@ -186,7 +186,7 @@ class Project
   has_many :invoices
 
   validates :name, presence: true
-  validates :name, uniqueness: {scope: :booking_portal_client_id, message: 'Project with same name is already present for this client'}, presence: true
+  validates :name, uniqueness: {scope: :booking_portal_client_id}, presence: true
   validates_uniqueness_of :rera_registration_no, allow_blank: true
   validates :enable_actual_inventory, array: { inclusion: {allow_blank: true, in: (User::ADMIN_ROLES + User::BUYER_ROLES) } }
   validates :ga_code, format: {with: /\Aua-\d{4,9}-\d{1,4}\z/i, message: 'is not valid'}, allow_blank: true
