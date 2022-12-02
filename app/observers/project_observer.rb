7803ab9e20ea87a2210e454dfb4f5a353d8783ed
shubham-alavni-selldo <shubham.alvani@sell.do>
2022-12-02 12:34:39 +0530

    [IRISMP-131] - payment changes added

--- app/observers/project_observer.rb
+++ app/observers/project_observer.rb
@@ -19,7 +19,7 @@ class ProjectObserver < Mongoid::Observer
     Template::AllotmentLetterTemplate.create(content: Template::AllotmentLetterTemplate.default_content, booking_portal_client_id: project.booking_portal_client_id, project_id: project.id)
     Template::BookingDetailFormTemplate.seed(project.id.to_s, project.booking_portal_client_id.to_s)
     # Create a default token type
-    token_type = project.token_types.create(name: 'Default', token_amount: (project.blocking_amount || project.booking_portal_client.blocking_amount), token_prefix: (project.name.gsub(/\s+/, '')[0..2].try(:upcase).presence || 'TKN'), token_seed: 0)
+    token_type = project.token_types.create(name: 'Default', token_amount: (project.blocking_amount || project.booking_portal_client.blocking_amount), token_prefix: (project.name.gsub(/\s+/, '')[0..2].try(:upcase).presence || 'TKN'), token_seed: 0,booking_portal_client_id: project.booking_portal_client_id)
     token_type.init if token_type.valid?
 
     # dump all the project ids to all the users that are not given project access
