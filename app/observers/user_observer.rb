4b0e91ea34a5bbd65917330d7084351c8a89f503
bonish-selldo <bonish.agarwal@sell.do>
2022-12-05 15:47:03 +0530

    [MP-143] - Deleted unnecessary services - Moved entire code for updating picklist values in a seperate service

--- app/observers/user_observer.rb
+++ app/observers/user_observer.rb
@@ -101,26 +101,7 @@ class UserObserver < Mongoid::Observer
         GenerateCoBrandingTemplatesWorker.new.perform(user.id)
       end
     end
-
-    if user.booking_portal_client.is_marketplace?&& user.role.in?(%w(cp_owner channel_partner)) && user.user_status_in_company == 'active' && (user.first_name_changed? || user.last_name_changed?)
-      booking_portal_client = user.booking_portal_client
-      admin_user = booking_portal_client.users.admin.ne(kylas_access_token: nil).first
-      deal_custom_field_id = booking_portal_client.kylas_custom_fields.dig(:deal, :id)
-      lead_custom_field_id = booking_portal_client.kylas_custom_fields.dig(:lead, :id)
-      meeting_custom_field_id = booking_portal_client.kylas_custom_fields.dig(:meeting, :id)
-
-      if deal_custom_field_id.present?
-        Kylas::UpdateDealPicklist.new(admin_user, user).call
-      end
-
-      if lead_custom_field_id.present?
-        Kylas::UpdateLeadPicklist.new(admin_user, user).call
-      end
-
-      if meeting_custom_field_id.present?
-        Kylas::UpdateMeetingPicklist.new(admin_user, user).call
-      end
-    end
+    Kylas::UpdatePicklistValues.new(user, user.changes).call if user.booking_portal_client.is_marketplace?
   end
 
   def after_update user
--- app/services/kylas/create_project_custom_field.rb
+++ app/services/kylas/create_project_custom_field.rb
@@ -69,15 +69,15 @@ module Kylas
       end
   
       def dump_kylas_field_ids(response)
-        booking_portal_client = user.booking_portal_client
-        booking_portal_client.set("kylas_custom_fields.meeting_project": ({id: response[:id]} rescue nil))
-        project_cf_id = booking_portal_client.kylas_custom_fields.dig('meeting_project', 'id')
+        client = user.booking_portal_client
+        client.set("kylas_custom_fields.meeting_project": ({id: response[:id]} rescue nil))
+        project_cf_id = client.kylas_custom_fields.dig('meeting_project', 'id')
         if project_cf_id.present?
           project_cf_response = Kylas::FetchMeetingCustomFieldDetails.new(user, project_cf_id).call
           if project_cf_response[:success]
             project_cf_response = project_cf_response.with_indifferent_access
             data = project_cf_response[:data]
-            booking_portal_client.set("kylas_custom_fields.meeting_project": booking_portal_client.kylas_custom_fields.dig(:meeting_project).merge(name: data.dig(:field, :name), picklist_id: data.dig(:field, :picklist, :id)))
+            client.set("kylas_custom_fields.meeting_project": client.kylas_custom_fields.dig(:meeting_project).merge(name: data.dig(:field, :name), picklist_id: data.dig(:field, :picklist, :id)))
           end
         end
       end
