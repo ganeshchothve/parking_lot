5d34c676ef9560629a22c319dafc68a31f355a95
bonish-selldo <bonish.agarwal@sell.do>
2022-12-06 15:53:17 +0530

    [MP-145] - removed access for sales and sales head to upload documents on booking

--- app/policies/admin/booking_detail_policy.rb
+++ app/policies/admin/booking_detail_policy.rb
@@ -180,7 +180,7 @@ class Admin::BookingDetailPolicy < BookingDetailPolicy
   end
 
   def asset_create?
-    %w[admin sales sales_admin account_manager account_manager_head account_manager_head billing_team cp_admin].include?(user.role)
+    %w[admin account_manager account_manager_head account_manager_head billing_team cp_admin].include?(user.role)
   end
 
   def asset_update?
