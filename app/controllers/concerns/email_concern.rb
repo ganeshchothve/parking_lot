d52c60138f42832ccb94da4137bb517e067285a3
bonish-selldo <bonish.agarwal@sell.do>
2022-12-13 19:28:42 +0530

    [Mp-Fixes] - Fixed email response issue

--- app/controllers/concerns/email_concern.rb
+++ app/controllers/concerns/email_concern.rb
@@ -23,7 +23,7 @@ module EmailConcern
       if @email.booking_portal_client.email_enabled? && @email.to.present?
         if @email.email_template && @email.email_template.try(:is_active?)
           email_response = Communication::Email::MailgunWorker.new.perform(@email.id.to_s)
-          if email_response.response["status"]
+          if (email_response.status == 'sent')
             format.html { redirect_to admin_emails_path, notice: t("controller.emails.resend_email.success") }
           else
             format.html { redirect_to admin_emails_path, alert: email_response.response["message"] rescue "Email not sent" }
