4277c32fe4b1508937c3d7e6d7dba86abed74648
shubham-alavni-selldo <shubham.alvani@sell.do>
2022-12-28 12:43:25 +0530

    [IRISMP-240] - KYC fields are not saved fixes

--- app/policies/bank_detail_policy.rb
+++ app/policies/bank_detail_policy.rb
@@ -1,5 +1,5 @@
 class BankDetailPolicy < ApplicationPolicy
   def permitted_attributes params={}
-    [:id, :name, :branch, :account_type, :account_holder_name, :ifsc_code, :account_number, :loan_required, :loan_amount, :loan_sanction_days, :zip, :_destroy]
+    [:id, :name, :branch, :account_type, :account_holder_name, :ifsc_code, :account_number, :loan_required, :loan_amount, :loan_sanction_days, :zip, :booking_portal_client_id, :_destroy]
   end
 end
