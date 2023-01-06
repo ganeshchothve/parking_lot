0f0794189c488b99d30e08597ab2e46395a0d73f
shubham-alavni-selldo <shubham.alvani@sell.do>
2023-01-06 17:25:40 +0530

    [IRISMP-292] - changes added

--- app/policies/admin/client_policy.rb
+++ app/policies/admin/client_policy.rb
@@ -43,7 +43,7 @@ class Admin::ClientPolicy < ClientPolicy
     when 'real_estate'
       if %w[superadmin admin].include?(user.role)
         attributes += [:twilio_account_sid, :twilio_auth_token, :twilio_virtual_number]
-        attributes += [general_user_request_categories: [], partner_regions: [], allowed_roles_to_create_lead: [], mask_lead_data_for_roles: [], team_lead_dashboard_access_roles: []]
+        attributes += [general_user_request_categories: [], partner_regions: [], mask_lead_data_for_roles: [], team_lead_dashboard_access_roles: []]
       end
     end
     if record.is_marketplace? && user.role.in?(%w(superadmin admin))
@@ -52,6 +52,7 @@ class Admin::ClientPolicy < ClientPolicy
     if user.role?('superadmin')
       attributes += [:industry, booking_portal_domains: []]
     end
+    attributes += [allowed_roles_to_create_lead: []]
     attributes.uniq
   end
 end
