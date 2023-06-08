a7e6661519ba04351d4cc54fc606ca5a4fef0d7e
ganeshchothve-selldo <ganesh.chothve@sell.do>
2023-06-08 19:29:32 +0530

    [irismp-414]-UI Issues

--- app/models/concerns/price_calculator.rb
+++ app/models/concerns/price_calculator.rb
@@ -37,9 +37,9 @@ module PriceCalculator
   def calculate_agreement_price
     agreement_price = base_price + total_agreement_costs
     if booking_detail_scheme
-      agreement_price += (booking_detail_scheme.payment_adjustments.where(field: "agreement_price").collect{|adj| adj.value(self)}.sum).round
+      agreement_price += (booking_detail_scheme.payment_adjustments.where(field: "agreement_price").collect{|adj| adj.value(self)}.sum)
     end
-    agreement_price
+    agreement_price.round
   end
 
   def calculate_all_inclusive_price
@@ -50,7 +50,7 @@ module PriceCalculator
     return all_incl_price if self.is_a? ProjectUnit
     all_incl_price += token_discount if token_discount.present?
     all_incl_price += variable_discount if variable_discount.present?
-    all_incl_price
+    all_incl_price.round
   end
 
   def base_price
