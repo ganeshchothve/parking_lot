5ce9b7049c72148ea28c9ce239db773a1d5faa7b
Milan Patel <milan.patel@sell.do>
2023-08-08 16:43:21 +0530

    HOTFIX - fix page break on register page

--- app/policies/third_party_reference_policy.rb
+++ app/policies/third_party_reference_policy.rb
@@ -2,7 +2,7 @@ class ThirdPartyReferencePolicy < ApplicationPolicy
 
   def permitted_attributes(params = {})
     attrs = []
-    attrs += [:id, :crm_id, :reference_id] if user.role.in?(%w(superadmin admin)) && !marketplace_client?
+    attrs += [:id, :crm_id, :reference_id] if user && user.role.in?(%w(superadmin admin)) && !marketplace_client?
     attrs
   end
 end
